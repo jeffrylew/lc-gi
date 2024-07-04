22b74f44c297e33e5994d0cc4628a883979e5168
Jeffry <jeffryklew@gmail.com>
2024-07-04 07:36:14 -0400

    Implement sort + greedy deletion discussion solution in minimum_difference_between_largest_and_smallest_value_in_three_moves.cpp

--- miscellaneous/minimum_difference_between_largest_and_smallest_value_in_three_moves.cpp
+++ miscellaneous/minimum_difference_between_largest_and_smallest_value_in_three_moves.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 
 #include <algorithm>
+#include <limits>
 #include <vector>
 
 //! @brief First attempt to get min diff after performing at most three moves
@@ -46,19 +47,64 @@ static int minDifferenceFA(std::vector<int> nums)
 
 } // static int minDifferenceFA( ...
 
+//! @brief Sort + Greedy deletion discussion solution
+//! @param[in] nums Vector of integers between -10^9 and 10^9
+//! @return Minimum difference between largest and smallest values of nums
+static int minDifferenceDS1(std::vector<int> nums)
+{
+    //! @details https://leetcode.com/problems
+    //!    /minimum-difference-between-largest-and-smallest-value-in-three-moves
+    //!
+    //!          Time complexity O(N * log N) where N = nums.size(). Sorting
+    //!          takes O(N * log N). The loop runs for 4 iterations, taking O(1)
+    //!          Space complexity O(N) or O(log N). When sorting nums, extra
+    //!          space is used and depends on the programming language. Python
+    //!          uses Timsort which combines merge sort and insertion sort and
+    //!          uses O(N) additional space. In C++, it is a hybrid of Quick
+    //!          Sort, Heap Sort, and Insertion Sort with a worst-case O(log N).
+
+    const auto nums_size = static_cast<int>(nums.size());
+
+    //! If vector has four or fewer elements, return 0
+    if (nums_size <= 4)
+    {
+        return 0;
+    }
+
+    std::sort(nums.begin(), nums.end());
+
+    int min_diff {std::numeric_limits<int>::max()};
+
+    //! Four scenarios to compute the minimum difference
+    //! - Remove three largest elements
+    //! - Remove two largest and smallest elements
+    //! - Remove largest and two smallest elements
+    //! - Remove three smallest elements
+    for (int left = 0; left < 4; ++left)
+    {
+        min_diff = std::min(nums[nums_size - 4 + left] - nums[left], min_diff);
+    }
+
+    return min_diff;
+
+} // static int minDifferenceDS1( ...
+
 TEST(MinDifferenceTest, SampleTest1)
 {
     EXPECT_EQ(0, minDifferenceFA({5, 3, 2, 4}));
+    EXPECT_EQ(0, minDifferenceDS1({5, 3, 2, 4}));
 }
 
 TEST(MinDifferenceTest, SampleTest2)
 {
     EXPECT_EQ(1, minDifferenceFA({1, 5, 0, 10, 14}));
+    EXPECT_EQ(1, minDifferenceDS1({1, 5, 0, 10, 14}));
 }
 
 TEST(MinDifferenceTest, SampleTest3)
 {
     EXPECT_EQ(0, minDifferenceFA({3, 100, 20}));
+    EXPECT_EQ(0, minDifferenceDS1({3, 100, 20}));
 }
 
 TEST(MinDifferenceTest, SampleTest4)
@@ -67,4 +113,5 @@ TEST(MinDifferenceTest, SampleTest4)
 
     EXPECT_EQ(33, minDifferenceFA(nums));
     EXPECT_NE(17, minDifferenceFA(nums));
+    EXPECT_EQ(17, minDifferenceDS1(nums));
 }
