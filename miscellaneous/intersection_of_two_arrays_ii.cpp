ef64005684e037f4d84aa20b4643c8d028a2078c
Jeffry <jeffryklew@gmail.com>
2024-07-04 09:11:34 -0400

    Implement hash map discussion solution in intersection_of_two_arrays_ii.cpp

--- miscellaneous/intersection_of_two_arrays_ii.cpp
+++ miscellaneous/intersection_of_two_arrays_ii.cpp
@@ -3,7 +3,7 @@
 #include <unordered_map>
 #include <vector>
 
-//! @brief Get intersection of two vectors
+//! @brief First attempt solution to get intersection of two vectors
 //! @param[in] nums1 Vector of integers from 0 to 1000
 //! @param[in] nums2 Vector of integers from 0 to 1000
 //! @return Intersection of nums1 and nums2
@@ -45,6 +45,46 @@ static std::vector<int> intersectFA(std::vector<int> nums1,
 
 } // static std::vector<int> intersectFA( ...
 
+//! @brief Hash map discussion solution to get intersection of two vectors
+//! @param[in] nums1 Vector of integers from 0 to 1000
+//! @param[in] nums2 Vector of integers from 0 to 1000
+//! @return Intersection of nums1 and nums2
+static std::vector<int> intersectDS1(std::vector<int> nums1,
+                                     std::vector<int> nums2)
+{
+    //! @details https://leetcode.com/problems/intersection-of-two-arrays-ii
+    //!
+    //!          Time complexity O(N + M) where N = nums1.size() and
+    //!          M = nums2.size().
+    //!          Space complexity O(min(N, M)). Use a hash map to store numbers
+    //!          and their counts from the smaller vector
+
+    //! Check which is smaller so can create hash map for smaller vector
+    if (nums1.size() > nums2.size())
+    {
+        nums1.swap(nums2);
+    }
+
+    std::unordered_map<int, int> nums1_counts {};
+    for (const int num : nums1)
+    {
+        ++nums1_counts[num];
+    }
+
+    int output_idx {};
+    for (const int num : nums2)
+    {
+        auto it = nums1_counts.find(num);
+        if (it != nums1_counts.end() && --it->second >= 0)
+        {
+            nums1[output_idx++] = num; 
+        }
+    }
+
+    return std::vector(nums1.begin(), nums1.begin() + output_idx);
+
+} // static std::vector<int> intersectDS1( ...
+
 TEST(IntersectTest, SampleTest1)
 {
     const std::vector<int> nums1 {1, 2, 2, 1};
@@ -52,6 +92,7 @@ TEST(IntersectTest, SampleTest1)
     const std::vector<int> expected_output {2, 2};
 
     EXPECT_EQ(expected_output, intersectFA(nums1, nums2));
+    EXPECT_EQ(expected_output, intersectDS1(nums1, nums2));
 }
 
 TEST(IntersectTest, SampleTest2)
@@ -61,4 +102,5 @@ TEST(IntersectTest, SampleTest2)
     const std::vector<int> expected_output {4, 9};
 
     EXPECT_EQ(expected_output, intersectFA(nums1, nums2));
+    EXPECT_EQ(expected_output, intersectDS1(nums1, nums2));
 }
