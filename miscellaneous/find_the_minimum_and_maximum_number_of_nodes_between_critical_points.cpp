b051f57a70c80262fcad300c6a283369156ff040
Jeffry <jeffryklew@gmail.com>
2024-07-06 06:25:38 -0400

    Implement one pass discussion solution in find_the_minimum_and_maximum_number_of_nodes_between_critical_points.cpp

--- miscellaneous/find_the_minimum_and_maximum_number_of_nodes_between_critical_points.cpp
+++ miscellaneous/find_the_minimum_and_maximum_number_of_nodes_between_critical_points.cpp
@@ -72,6 +72,72 @@ static std::vector<int> nodesBetweenCriticalPointsFA(ListNode* head)
 
 } // static std::vector<int> nodesBetweenCriticalPointsFA( ...
 
+//! @brief One pass discussion solution
+//! @param[in] head Pointer to head of linked list
+//! @return Vector containing {minDistance, maxDistance} or {-1, -1} if < 2 pts
+static std::vector<int> nodesBetweenCriticalPointsDS(ListNode* head)
+{
+    //! @details https://leetcode.com/problems/
+    //!     find-the-minimum-and-maximum-number-of-nodes-between-critical-points
+    //!
+    //!          Time complexity O(N) where N = number of nodes in linked list.
+    //!          The algorithm traverses the list only once.
+    //!          Space complexity O(1), algorithm does not utilize any
+    //!          additional data structures.
+
+    std::vector<int> result {-1, -1};
+
+    //! Initialize min distance
+    int min_distance {std::numeric_limits<int>::max()};
+
+    //! Pointers to track the previous node, current node, and indices
+    ListNode* previous_node = head;
+    ListNode* current_node  = head->next;
+
+    int current_index {1};
+    int previous_critical_index {};
+    int first_critical_index {};
+
+    while (current_node->next != nullptr)
+    {
+        //! Check if current node is a local maxima or minima
+        if ((current_node->val < previous_node->val
+             && current_node->val < current_node->next->val)
+            || (current_node->val > previous_node->val
+                && current_node->val > current_node->next->val))
+        {
+            if (previous_critical_index == 0)
+            {
+                //! This is the first critical point found
+                previous_critical_index = current_index;
+                first_critical_index    = current_index;
+            }
+            else
+            {
+                //! Calculate the minimum distance between critical points
+                min_distance = std::min(current_index - previous_critical_index,
+                                        min_distance);
+                previous_critical_index = current_index;
+            }
+        }
+
+        //! Move to the next node and update indices
+        ++current_index;
+        previous_node = current_node;
+        current_node  = current_node->next;
+    }
+
+    //! If at least two critical points were found
+    if (min_distance != std::numeric_limits<int>::max())
+    {
+        const int max_distance {previous_critical_index - first_critical_index};
+        result = {min_distance, max_distance};
+    }
+
+    return result;
+
+} // static std::vector<int> nodesBetweenCriticalPointsDS( ...
+
 TEST(NodesBetweenCriticalPointsTest, SampleTest1)
 {
     const ListNode         one {1};
@@ -79,6 +145,7 @@ TEST(NodesBetweenCriticalPointsTest, SampleTest1)
     const std::vector<int> expected_output {-1, -1};
 
     EXPECT_EQ(expected_output, nodesBetweenCriticalPointsFA(&three));
+    EXPECT_EQ(expected_output, nodesBetweenCriticalPointsDS(&three));
 }
 
 TEST(NodesBetweenCriticalPointsTest, SampleTest2)
@@ -93,4 +160,5 @@ TEST(NodesBetweenCriticalPointsTest, SampleTest2)
     const std::vector<int> expected_output {1, 3};
 
     EXPECT_EQ(expected_output, nodesBetweenCriticalPointsFA(&five_1));
+    EXPECT_EQ(expected_output, nodesBetweenCriticalPointsDS(&five_1));
 }
