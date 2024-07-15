dcd6a09be8a89b44f3d8d0030d31a13d4f065112
Jeffry <jeffryklew@gmail.com>
2024-07-14 20:11:44 -0400

    Implement straightforward way discussion solution in reverse_substrings_between_each_pair_of_parentheses.cpp

--- miscellaneous/reverse_substrings_between_each_pair_of_parentheses.cpp
+++ miscellaneous/reverse_substrings_between_each_pair_of_parentheses.cpp
@@ -92,22 +92,81 @@ static std::string reverseParenthesesFA(std::string s)
 
 } // static std::string reverseParenthesesFA( ...
 
+
+//! @brief Straightforward way discussion solution
+//! @param[in] s std::string consisting of lower case English letters/brackets
+//! @return std::string with reverse strings in each pair of matching brackets
+static std::string reverseParenthesesDS1(std::string s)
+{
+    //! @details https://leetcode.com/problems/
+    //!          reverse-substrings-between-each-pair-of-parentheses
+    //!
+    //!          Time complexity O(N ^ 2) where N = s.size(). The algorithm
+    //!          iterates through each input string char once. For each char,
+    //!          - If it is (, push to stack in O(1)
+    //!          - If it is ), pop from stack and reverse portion of result
+    //!            - Popping is O(1)
+    //!            - Reverse operation can take up to O(N) in worst case when
+    //!              reversing entire string
+    //!          - For other characters, append to result in O(1) amortized
+    //!          Worst-case scenario occurs when have to reverse large portions
+    //!          of string multiple times. In worst case, have to reverse entire
+    //!          string for each closing parenthesis.
+    //!          Space complexity O(N). In worst case when all chars are opening
+    //!          parentheses, stack could take O(N) space (technically O(N / 2)
+    //!          = O(N) since the problem guarantees matching parentheses). The
+    //!          reverse function typically doesn't use extra space proportional
+    //!          to the input size.
+
+    std::string     result {};
+    std::stack<int> open_parentheses_indices {};
+
+    for (const char current_char : s)
+    {
+        if (current_char == '(')
+        {
+            //! Store the current length as start index for future reversal
+            open_parentheses_indices.push(static_cast<int>(std::ssize(result)));
+        }
+        else if (current_char == ')')
+        {
+            const int start_idx {open_parentheses_indices.top()};
+            open_parentheses_indices.pop();
+
+            //! Reverse substring between matching parentheses
+            std::reverse(result.begin() + start_idx, result.end());
+        }
+        else
+        {
+            //! Character is not a parenthesis, append to processed string
+            result += current_char;
+        }
+    }
+
+    return result;
+
+} // static std::string reverseParenthesesDS1( ...
+
 TEST(ReverseParenthesesTest, SampleTest1)
 {
     EXPECT_EQ("dcba", reverseParenthesesFA("(abcd)"));
+    EXPECT_EQ("dcba", reverseParenthesesDS1("(abcd)"));
 }
 
 TEST(ReverseParenthesesTest, SampleTest2)
 {
     EXPECT_EQ("iloveu", reverseParenthesesFA("(u(love)i)"));
+    EXPECT_EQ("iloveu", reverseParenthesesDS1("(u(love)i)"));
 }
 
 TEST(ReverseParenthesesTest, SampleTest3)
 {
     EXPECT_EQ("leetcode", reverseParenthesesFA("(ed(et(oc))el)"));
+    EXPECT_EQ("leetcode", reverseParenthesesDS1("(ed(et(oc))el)"));
 }
 
 TEST(ReverseParenthesesTest, SampleTest4)
 {
     EXPECT_NE("yfgnxf", reverseParenthesesFA("yfgnxf"));
+    EXPECT_EQ("yfgnxf", reverseParenthesesDS1("yfgnxf"));
 }
