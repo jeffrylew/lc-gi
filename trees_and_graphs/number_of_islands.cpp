439b0f16387b61939f9fb151b4ca8bd560de02e4
Jeffry <jeffryklew@gmail.com>
2024-07-25 07:06:15 -0400

    Implement DFS discussion solution in trees_and_graphs/number_of_islands.cpp

--- trees_and_graphs/number_of_islands.cpp
+++ trees_and_graphs/number_of_islands.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 
+#include <functional>
 #include <queue>
 #include <utility>
 #include <vector>
@@ -30,7 +31,7 @@ static int numIslandsFA(const std::vector<std::vector<char>>& grid)
 
     int num_islands {};
 
-    std::vector<std::pair<int, int>> directions {
+    const std::vector<std::pair<int, int>> directions {
         {0, -1}, {0, 1}, {-1, 0}, {1, 0}};
 
     std::queue<std::pair<int, int>> coords {};
@@ -76,6 +77,93 @@ static int numIslandsFA(const std::vector<std::vector<char>>& grid)
 
 } // static int numIslandsFA( ...
 
+//! @brief DFS discussion solution
+//! @param[in] grid Reference to an M x N 2D grid of 1s (land) and 0s (water)
+//! @return Number of islands
+static int numIslandsDS1(const std::vector<std::vector<char>>& grid)
+{
+    //! @details https://leetcode.com/problems/number-of-islands/description
+    //!
+    //!          Time complexity O(M * N) where M = number of rows and
+    //!          N = number of cols
+    //!          Space complexity O(M * N) in the case that the grid map is
+    //!          filled with lands where DFS is M x N deep. This also excludes
+    //!          the use of grid_cpy.
+
+    auto grid_cpy = grid;
+
+    const auto num_rows = static_cast<int>(grid_cpy.size());
+    if (num_rows == 0)
+    {
+        return 0;
+    }
+    const auto num_cols = static_cast<int>(grid_cpy[0].size());
+
+    const std::vector<std::pair<int, int>> directions {
+        {-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+    const auto is_valid = [&](int row, int col) {
+        return row >= 0 && row < num_rows  && col >= 0 && col < num_cols;
+    };
+    
+    std::function<void(int, int)> dfs = [&](int row_in, int col_in) {
+        grid_cpy[row_in][col_in] = '0';
+
+        for (const auto& [drow, dcol] : directions)
+        {
+            const int next_row {row_in + drow};
+            const int next_col {col_in + dcol};
+
+            if (is_valid(next_row, next_col)
+                && grid_cpy[next_row][next_col] == '1')
+            {
+                dfs(next_row, next_col);
+            }
+        }
+
+        /*
+         * Actual solution from editorial, except grid used instead of grid_cpy
+         *
+        grid_cpy[row_in][col_in] = '0';
+
+        if (row_in - 1 >= 0 && grid_cpy[row_in - 1][col_in] == '1')
+        {
+            dfs(row_in - 1, col_in);
+        }
+        if (row_in + 1 < num_rows && grid_cpy[row_in + 1][col_in] == '1')
+        {
+            dfs(row_in + 1, col_in);
+        }
+        if (col_in - 1 >= 0 && grid_cpy[row_in][col_in - 1] == '1')
+        {
+            dfs(row_in, col_in - 1);
+        }
+        if (col_in + 1 < num_cols && grid_cpy[row_in][col_in + 1] == '1')
+        {
+            dfs(row_in, col_in + 1);
+        }
+         */
+    };
+
+    int num_islands {};
+
+    for (int row = 0; row < num_rows; ++row)
+    {
+        for (int col = 0; col < num_cols; ++col)
+        {
+            if (grid_cpy[row][col] == '1')
+            {
+                ++num_islands;
+
+                dfs(row, col);
+            }
+        }
+    }
+
+    return num_islands;
+
+} // static int numIslandsDS1( ...
+
 TEST(NumIslandsTest, SampleTest1)
 {
     const std::vector<std::vector<char>> grid {
@@ -85,6 +173,7 @@ TEST(NumIslandsTest, SampleTest1)
         {'0', '0', '0', '0', '0'}};
 
     EXPECT_EQ(1, numIslandsFA(grid));
+    EXPECT_EQ(1, numIslandsDS1(grid));
 }
 
 TEST(NumIslandsTest, SampleTest2)
@@ -96,6 +185,7 @@ TEST(NumIslandsTest, SampleTest2)
         {'0', '0', '0', '1', '1'}};
 
     EXPECT_EQ(1, numIslandsFA(grid));
+    EXPECT_EQ(1, numIslandsDS1(grid));
 }
 
 TEST(NumIslandsTest, SampleTest3)
