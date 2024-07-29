eb02ae84b13214f4e897f780adbf55f41604e0c7
Jeffry <jeffryklew@gmail.com>
2024-07-29 19:22:42 -0400

    Implement binary search discussion solution in trees_and_graphs/count_complete_tree_nodes.cpp

--- trees_and_graphs/count_complete_tree_nodes.cpp
+++ trees_and_graphs/count_complete_tree_nodes.cpp
@@ -100,6 +100,98 @@ static int countNodesDS1(TreeNode* root)
         : 0;
 }
 
+//! @brief Binary search discussion solution
+//! @param[in] root Pointer to root of complete binary tree
+//! @return Number of nodes in complete binary tree
+static int countNodesDS2(TreeNode* root)
+{
+    //! @details https://leetcode.com/problems/count-complete-tree-nodes
+    //!
+    //!          Time complexity O(depth ^ 2) = O(log^2 N) where N = number of
+    //!          nodes in the tree
+    //!          Space complexity O(1)
+
+    if (root == nullptr)
+    {
+        //! Tree is empty
+        return 0;
+    }
+
+    const auto compute_height = [](TreeNode* node) -> int {
+        int height {};
+
+        while (node->left != nullptr)
+        {
+            node = node->left;
+            ++height;
+        }
+
+        return height;
+    };
+
+    const int depth {compute_height(root)};
+    if (depth == 0)
+    {
+        //! Tree contains 1 node
+        return 1;
+    }
+
+    //! @brief Check if last level node with index idx exists
+    //! @param[in] idx  Index of node in last level to check if it exists
+    //! @param[in] node Pointer to TreeNode in first level to search from
+    //! @return True if the last level node at index idx exists, else false
+    const auto exists = [&depth](int idx, TreeNode* node) -> bool {
+        //! Last level nodes enumerated from 0 to 2 ^ depth - 1 (left -> right)
+        int left {};
+        int right {std::pow(2, depth) - 1}; // Index of last node in last level
+        int pivot {};
+
+        for (int i = 0; i < depth; ++i)
+        {
+            pivot = left + (right - left) / 2;
+
+            if (idx <= pivot)
+            {
+                node  = node->left;
+                right = pivot;
+            }
+            else
+            {
+                node = node->right;
+                left = pivot + 1;
+            }
+        }
+
+        return node != nullptr;
+
+    }; // const auto exists = ...
+
+    //! Perform binary search to check how many nodes exist
+    //! Last level nodes enumerated from 0 to 2 ^ depth - 1 (left -> right)
+    int left {1};
+    int right {std::pow(2, depth) - 1};
+    int pivot {};
+
+    while (left <= right)
+    {
+        pivot = left + (right - left) / 2;
+
+        if (exists(pivot, root))
+        {
+            left = pivot + 1;
+        }
+        else
+        {
+            right = pivot - 1;
+        }
+    }
+
+    //! The tree contains 2 ^ depth - 1 nodes on the first (depth - 1) levels
+    //! and left nodes on the last level
+    return std::pow(2, depth) - 1 + left;
+
+} // static int countNodesDS2( ...
+
 TEST(CountNodesTest, SampleTest1)
 {
     const TreeNode six {6};
@@ -113,12 +205,14 @@ TEST(CountNodesTest, SampleTest1)
 
     EXPECT_EQ(6, countNodesFA(&one));
     EXPECT_EQ(6, countNodesDS1(&one));
+    EXPECT_EQ(6, countNodesDS2(&one));
 }
 
 TEST(CountNodesTest, SampleTest2)
 {
     EXPECT_EQ(0, countNodesFA(nullptr));
     EXPECT_EQ(0, countNodesDS1(nullptr));
+    EXPECT_EQ(0, countNodesDS2(nullptr));
 }
 
 TEST(CountNodesTest, SampleTest3)
@@ -127,4 +221,5 @@ TEST(CountNodesTest, SampleTest3)
 
     EXPECT_EQ(1, countNodesFA(&one));
     EXPECT_EQ(1, countNodesDS1(&one));
+    EXPECT_EQ(1, countNodesDS2(&one));
 }
