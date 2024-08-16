ecb9526f2fc82d48486fdefc8797f036888833ac
Jeffry <jeffryklew@gmail.com>
2024-08-16 18:40:19 -0400

    Implement simulation discussion solution in miscellaneous/lemonade_change.cpp

--- miscellaneous/lemonade_change.cpp
+++ miscellaneous/lemonade_change.cpp
@@ -60,12 +60,80 @@ static bool lemonadeChangeFA(std::vector<int> bills)
 
 } // static bool lemonadeChangeFA( ...
 
+//! @brief Simulation discussion solution
+//! @param[in] bills Vector of 5, 10, and 20 dollar bills from customers
+//! @return True if can provide every customer with correct change, else false
+static bool lemonadeChangeDS(std::vector<int> bills)
+{
+    //! @details https://leetcode.com/problems/lemonade-change/description/
+    //!
+    //!          Time complexity O(N) where N = bills.size(). The algorithm
+    //!          loops over bills once, taking O(N). All operations within the
+    //!          loop are constant time.
+    //!          Space complexity O(1). The algorithm does not use additional
+    //!          data structures that scale with the input size.
+
+    //! Count of $5 and $10 bills in hand
+    int five_dollar_bills {};
+    int ten_dollar_bills {};
+
+    //! Iterate through each customer's bill
+    for (const int customer_bill : bills)
+    {
+        if (customer_bill == 5)
+        {
+            //! Add it to our count
+            ++five_dollar_bills;
+        }
+        else if (customer_bill == 10)
+        {
+            //! Need to give $5 change
+            if (five_dollar_bills > 0)
+            {
+                --five_dollar_bills;
+                ++ten_dollar_bills;
+            }
+            else
+            {
+                //! Can't provide change, return false
+                return false;
+            }
+        }
+        else
+        {
+            //! Need to give $15 change
+            if (ten_dollar_bills > 0 && five_dollar_bills > 0)
+            {
+                //! Give change as one $10 and one $5
+                --five_dollar_bills;
+                --ten_dollar_bills;
+            }
+            else if (five_dollar_bills >= 3)
+            {
+                //! Give change as three $5
+                five_dollar_bills -= 3;
+            }
+            else
+            {
+                //! Can't provide change, return false
+                return false;
+            }
+        }
+    }
+
+    //! If made it through all customers, return true
+    return true;
+
+} // static bool lemonadeChangeDS( ...
+
 TEST(LemonadeChangeTest, SampleTest1)
 {
     EXPECT_TRUE(lemonadeChangeFA({5, 5, 5, 10, 20}));
+    EXPECT_TRUE(lemonadeChangeDS({5, 5, 5, 10, 20}));
 }
 
 TEST(LemonadeChangeTest, SampleTest2)
 {
     EXPECT_FALSE(lemonadeChangeFA({5, 5, 10, 10, 20}));
+    EXPECT_FALSE(lemonadeChangeDS({5, 5, 10, 10, 20}));
 }
