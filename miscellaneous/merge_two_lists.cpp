81d2b35b3e3b28a11473bed89f5387f75304374c
Jeffry <jeffryklew@gmail.com>
2024-08-23 22:01:35 -0400

    Fix first attempt solution and add tests/big O analysis to miscellaneous/merge_two_lists.cpp

--- miscellaneous/merge_two_lists.cpp
+++ miscellaneous/merge_two_lists.cpp
@@ -2,12 +2,18 @@
 
 #include <gtest/gtest.h>
 
-//! @brief Merge two sorted linked lists into one sorted list
+//! @brief First attempt to merge two sorted linked lists into one sorted list
 //! @param[in] list1 Head of first sorted linked list
 //! @param[in] list2 Head of second sorted linked list
 //! @return Head of the merged linked list
 static ListNode* mergeTwoListsFA(ListNode* list1, ListNode* list2)
 {
+    //! @details https://leetcode.com/problems/merge-two-sorted-lists
+    //!
+    //!          Time complexity O(M + N) where M = number of nodes in list1 and
+    //!          N = number of nodes in list2.
+    //!          Space complexity O(1)
+
     if (list1 == nullptr && list2 == nullptr)
     {
         return nullptr;
@@ -38,15 +44,25 @@ static ListNode* mergeTwoListsFA(ListNode* list1, ListNode* list2)
 
         if (val1 <= val2)
         {
-            prev1       = list1;
-            list1->next = list2;
-            list1       = next1;
+            prev1 = list1;
+
+            if (next1 == nullptr ||  list2->val < next1->val)
+            {
+                list1->next = list2;
+            }
+
+            list1 = next1;
         }
         else
         {
-            prev2       = list2;
-            list2->next = list1;
-            list2       = next2;
+            prev2 = list2;
+
+            if (next2 == nullptr || list1->val <= next2->val)
+            {
+                list2->next = list1;
+            }
+
+            list2 = next2;
         }
     }
 
@@ -63,3 +79,43 @@ static ListNode* mergeTwoListsFA(ListNode* list1, ListNode* list2)
     return head;
 
 } // static ListNode* mergeTwoListsFA( ...
+
+TEST(MergeTwoListsTest, SampleTest1)
+{
+    ListNode four_1 {4};
+    ListNode two {2, &four_1};
+    ListNode one_1 {1, &two};
+
+    ListNode four_2 {4};
+    ListNode three {3, &four_2};
+    ListNode one_2 {1, &three};
+
+    const ListNode out_four_2 {4};
+    const ListNode out_four_1 {4, &out_four_2};
+    const ListNode out_three {3, &out_four_1};
+    const ListNode out_two {2, &out_three};
+    const ListNode out_one_2 {1, &out_two};
+    const ListNode out_one_1 {1, &out_one_2};
+
+    auto* result          = mergeTwoListsFA(&one_1, &one_2);
+    auto* expected_output = &out_one_1;
+
+    while (result != nullptr && expected_output != nullptr)
+    {
+        EXPECT_EQ(result->val, expected_output->val);
+        result          = result->next;
+        expected_output = expected_output->next;
+    }
+}
+
+TEST(MergeTwoListsTest, SampleTest2)
+{
+    EXPECT_EQ(nullptr, mergeTwoListsFA(nullptr, nullptr));
+}
+
+TEST(MergeTwoListsTest, SampleTest3)
+{
+    const ListNode zero {0};
+
+    EXPECT_EQ(zero->val, mergeTwoListsFA(nullptr, &zero)->val);
+}
