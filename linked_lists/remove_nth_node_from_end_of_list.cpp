e93cf8eb3897864730fb01b207ec3c09421c6517
Jeffry <jeffryklew@gmail.com>
2024-08-27 04:25:14 -0400

    Fix first attempt solution in linked_lists/remove_nth_node_from_end_of_list.cpp

--- linked_lists/remove_nth_node_from_end_of_list.cpp
+++ linked_lists/remove_nth_node_from_end_of_list.cpp
@@ -2,8 +2,17 @@
 
 #include <gtest/gtest.h>
 
+//! @brief First attempt to remove nth node from end of list and return head
+//! @param[in] head Pointer to head of linked list
+//! @param[in] n    Number of node from end to remove
+//! @return Pointer to head of list after removal
 static ListNode* removeNthFromEndFA(ListNode* head, int n)
 {
+    //! @details https://leetcode.com/problems/remove-nth-node-from-end-of-list
+    //!
+    //!          Time complexity O(N) where N = number of nodes in list
+    //!          Space complexity O(1)
+
     ListNode* prev {nullptr};
     ListNode* curr {nullptr};
     ListNode* n_ahead = head;
@@ -16,6 +25,10 @@ static ListNode* removeNthFromEndFA(ListNode* head, int n)
 
     if (n_ahead == nullptr)
     {
+        if (head->next != nullptr)
+        {
+            return head->next;
+        }
         return nullptr;
     }
 
@@ -27,12 +40,34 @@ static ListNode* removeNthFromEndFA(ListNode* head, int n)
         curr    = curr->next;
     }
 
+    if (curr == nullptr)
+    {
+        prev->next = nullptr;
+        return prev;
+    }
     if (prev != nullptr)
     {
         prev->next = curr->next;
     }
-    delete curr;
+    curr = nullptr;
 
     return head;
 
 } // static ListNode* removeNthFromEndFA( ...
+
+TEST(RemoveNthFromEndTest, SampleTest2)
+{
+    ListNode one {1};
+
+    EXPECT_EQ(nullptr, removeNthFromEndFA(&one, 1));
+}
+
+TEST(RemoveNthFromEndTest, SampleTest3)
+{
+    ListNode two {2};
+    ListNode one {1, &two};
+
+    ListNode expected_one {1};
+
+    EXPECT_EQ(expected_one->val, removeNthFromEndFA(&one, 1)->val);
+}
