b0fe179a91db5596238bcc895492203c0091af57
Jeffry <jeffryklew@gmail.com>
2024-09-09 18:51:01 -0400

    Implement two pointer approach discussion solution in container_with_most_water.cpp

--- arrays_and_strings/container_with_most_water.cpp
+++ arrays_and_strings/container_with_most_water.cpp
@@ -4,7 +4,8 @@
 #include <vector>
 
 //! @brief First attempt to get max amount of water a container can store
-//! @param[in] height Vector of heights of length 
+//! @param[in] height Vector of heights of length N
+//! @return Max amount of water a container can store
 static int maxAreaFA(std::vector<int> height)
 {
     //! @details https://leetcode.com/problems/container-with-most-water
@@ -41,7 +42,8 @@ static int maxAreaFA(std::vector<int> height)
 } // static int maxAreaFA( ...
 
 //! @brief Brute force discussion solution
-//! @param[in] height Vector of heights of length 
+//! @param[in] height Vector of heights of length N
+//! @return Max amount of water a container can store
 static int maxAreaDS1(std::vector<int> height)
 {
     //! @details https://leetcode.com/problems/container-with-most-water
@@ -69,14 +71,51 @@ static int maxAreaDS1(std::vector<int> height)
 
 } // static int maxAreaDS1( ...
 
+//! @brief Two pointer approach discussion solution
+//! @param[in] height Vector of heights of length N
+//! @return Max amount of water a container can store
+static int maxAreaDS2(std::vector<int> height)
+{
+    //! @details https://leetcode.com/problems/container-with-most-water
+    //!
+    //!          Time complexity O(N) where N = height.size(). Single pass.
+    //!          Space complexity O(1)
+
+    int max_area {};
+    int left {};
+    int right {static_cast<int>(std::ssize(height)) - 1};
+
+    while (left < right)
+    {
+        const int width {right - left};
+
+        max_area = std::max(max_area,
+                            std::min(height[left], height[right]) * width);
+
+        if (height[left] <= height[right])
+        {
+            ++left;
+        }
+        else
+        {
+            --right;
+        }
+    }
+
+    return max_area;
+
+} // static int maxAreaDS2( ...
+
 TEST(MaxAreaTest, SampleTest1)
 {
     EXPECT_EQ(49, maxAreaFA({1, 8, 6, 2, 5, 4, 8, 3, 7}));
     EXPECT_EQ(49, maxAreaDS1({1, 8, 6, 2, 5, 4, 8, 3, 7}));
+    EXPECT_EQ(49, maxAreaDS2({1, 8, 6, 2, 5, 4, 8, 3, 7}));
 }
 
 TEST(MaxAreaTest, SampleTest2)
 {
     EXPECT_EQ(1, maxAreaFA({1, 1}));
     EXPECT_EQ(1, maxAreaDS1({1, 1}));
+    EXPECT_EQ(1, maxAreaDS2({1, 1}));
 }
