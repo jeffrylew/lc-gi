3500ff12bb272c1110460c213a8c3d53196145d3
Jeffry <jeffryklew@gmail.com>
2024-09-11 20:20:14 -0400

    Implement two pointers discussion solution in arrays_and_strings/3sum.cpp

--- arrays_and_strings/3sum.cpp
+++ arrays_and_strings/3sum.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 
 #include <algorithm>
+#include <functional>
 #include <set>
 #include <tuple>
 #include <unordered_map>
@@ -75,12 +76,78 @@ static std::vector<std::vector<int>> threeSumFA(std::vector<int> nums)
 
 } // static std::vector<std::vector<int>> threeSumFA( ...
 
+//! @brief Two pointers discussion solution
+//! @param[in] nums Vector of integers of size in [3, 3000]
+//! @return Vector of unique triplets with zero sum
+static std::vector<std::vector<int>> threeSumDS1(std::vector<int> nums)
+{
+    //! @details https://leetcode.com/problems/3sum/description/
+    //!
+    //!          Time complexity O(N ^ 2). two_sum_ii is O(N) and we call it
+    //!          N times. Sorting takes O(N log N).
+    //!          Space complexity O(log N) to O(N) depending on sorting
+    //!          algorithm implementation. For std::sort, average is O(log N).
+
+    std::sort(nums.begin(), nums.end());
+    const auto nums_size = static_cast<int>(std::ssize(nums));
+
+    std::vector<std::vector<int>> res {};
+
+    const auto two_sum_ii = [&](int idx_in) {
+        int lo {idx_in + 1};
+        int hi {nums_size - 1};
+
+        while (lo < hi)
+        {
+            const int sum {nums[idx_in] + nums[lo] + nums[hi]};
+            if (sum < 0)
+            {
+                ++lo;
+            }
+            else if (sum > 0)
+            {
+                --hi;
+            }
+            else
+            {
+                //! Found triplet, increment lo and decrement hi
+                res.push_back({nums[idx_in], nums[lo++], nums[hi--]});
+
+                while (lo < hi && nums[lo] == nums[lo - 1])
+                {
+                    //! Increment lo to avoid duplicates in result
+                    ++lo;
+                }
+            }
+        }
+    };
+
+    for (int idx = 0; idx < nums_size; ++idx)
+    {
+        if (nums[idx] > 0)
+        {
+            //! Remaining vlaues cannot sum to zero
+            break;
+        }
+
+        //! Skip if current value is same as previous
+        if (idx == 0 || nums[idx - 1] != nums[idx])
+        {
+            two_sum_ii(idx);
+        }
+    }
+
+    return res;
+
+} // static std::vector<std::vector<int>> threeSumDS1( ...
+
 TEST(ThreeSumTest, SampleTest1)
 {
     const std::vector<std::vector<int>> expected_output {
         {-1, -1, 2}, {-1, 0, 1}};
 
     EXPECT_EQ(expected_output, threeSumFA({-1, 0, 1, 2, -1, -4}));
+    EXPECT_EQ(expected_output, threeSumDS1({-1, 0, 1, 2, -1, -4}));
 }
 
 TEST(ThreeSumTest, SampleTest2)
@@ -88,6 +155,7 @@ TEST(ThreeSumTest, SampleTest2)
     const std::vector<std::vector<int>> expected_output {{0, 1, 1}};
 
     EXPECT_EQ(expected_output, threeSumFA({0, 1, 1}));
+    EXPECT_EQ(expected_output, threeSumDS1({0, 1, 1}));
 }
 
 TEST(ThreeSumTest, SampleTest3)
@@ -95,6 +163,7 @@ TEST(ThreeSumTest, SampleTest3)
     const std::vector<std::vector<int>> expected_output {{0, 0, 0}};
 
     EXPECT_EQ(expected_output, threeSumFA({0, 0, 0}));
+    EXPECT_EQ(expected_output, threeSumDS1({0, 0, 0}));
 }
 
 TEST(ThreeSumTest, SampleTest4)
@@ -121,4 +190,5 @@ TEST(ThreeSumTest, SampleTest4)
 
     EXPECT_NE(expected_output, threeSumFA(nums));
     EXPECT_EQ(first_attempt_output, threeSumFA(nums));
+    EXPECT_EQ(expected_output, threeSumDS1(nums));
 }
