b7172cc865e82b0ad92a5e79a6f71ca85f371243
Jeffry <jeffryklew@gmail.com>
2024-10-07 21:33:01 -0400

    Implement rotate groups of four cells discussion solution in rotate_image.cpp

--- arrays_and_strings/rotate_image.cpp
+++ arrays_and_strings/rotate_image.cpp
@@ -87,15 +87,54 @@ static void rotateFA(std::vector<std::vector<int>>& matrix)
 
 } // static void rotateFA( ...
 
+//! @brief Rotate groups of four cells discussion solution
+//! @param[in, out] matrix Reference to n x n 2D matrix representing an image
+static void rotateDS1(std::vector<std::vector<int>>& matrix)
+{
+    //! @details https://leetcode.com/problems/rotate-image/description/
+    //!
+    //!          Time complexity O(N ^ 2) where N = matrix.size()
+    //!          Space complexity O(1)
+
+    const auto N = static_cast<int>(std::ssize(matrix));
+
+    for (int i = 0; i < (N + 1) / 2; ++i)
+    {
+        for (int j = 0; j < N / 2; ++j)
+        {
+            //! Save bottom left corner (initially).
+            const int temp {matrix[N - 1 - j][i]};
+
+            //! Set bottom left corner to bottom right corner (initially)
+            matrix[N - 1 - j][i] = matrix[N - 1 - i][N - j - 1];
+
+            //! Set bottom right corner to top right corner (initially)
+            matrix[N - 1 - i][N - j - 1] = matrix[j][N - 1 - i];
+
+            //! Set top right corner to top left corner (initially)
+            matrix[j][N - 1 - i] = matrix[i][j];
+
+            //! Set top left corner to initial bottom left corner value
+            matrix[i][j] = temp;
+        }
+    }
+
+} // static void rotateDS1( ...
+
 TEST(RotateTest, SampleTest1)
 {
     std::vector<std::vector<int>> matrixFA {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+
+    auto matrixDS1 = matrixFA;
 
     const std::vector<std::vector<int>> expected_output {
         {7, 4, 1}, {8, 5, 2}, {9, 6, 3}};
 
     rotateFA(matrixFA);
     EXPECT_EQ(expected_output, matrixFA);
+
+    rotateDS1(matrixDS1);
+    EXPECT_EQ(expected_output, matrixDS1);
 }
 
 TEST(RotateTest, SampleTest2)
@@ -103,9 +142,14 @@ TEST(RotateTest, SampleTest2)
     std::vector<std::vector<int>> matrixFA {
         {5, 1, 9, 11}, {2, 4, 8, 10}, {13, 3, 6, 7}, {15, 14, 12, 16}};
 
+    auto matrixDS1 = matrixFA;
+
     const std::vector<std::vector<int>> expected_output {
         {15, 13, 2, 5}, {14, 3, 4, 1}, {12, 6, 8, 9}, {16, 7, 10, 11}};
 
     rotateFA(matrixFA);
     EXPECT_EQ(expected_output, matrixFA);
+
+    rotateDS1(matrixDS1);
+    EXPECT_EQ(expected_output, matrixDS1);
 }
