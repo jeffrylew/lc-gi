3d8d2ea947f48264950ae1c754e10190c693fff5
Jeffry <jeffryklew@gmail.com>
2024-10-12 10:36:26 -0400

    Implement schoolbook addition with carry discussion solution in plus_one.cpp

--- arrays_and_strings/plus_one.cpp
+++ arrays_and_strings/plus_one.cpp
@@ -39,11 +39,44 @@ static std::vector<int> plusOneFA(std::vector<int> digits)
 
 } // static std::vector<int> plusOneFA( ...
 
+//! @brief Schoolbook addition with carry discussion solution
+//! @param[in] digits Vector of digits from most to least significant from LtoR
+//! @return Resulting vector of digits after incrementing by one
+static std::vector<int> plusOneDS(std::vector<int> digits)
+{
+    //! @details https://leetcode.com/problems/plus-one/description/
+    //!
+    //!          Time complexity O(N) where N = digits.size() for one pass
+    //!          Space complexity O(N). Although we perform the operation
+    //!          in-place, in the worst scenario we need to allocate an
+    //!          intermediate space to hold N + 1 elements.
+
+    const auto digits_size = static_cast<int>(std::ssize(digits));
+
+    //! Move along input vector starting from the end
+    for (int idx = digits_size - 1; idx >= 0; --idx)
+    {
+        if (digits[idx] != 9)
+        {
+            ++digits[idx];
+            return digits;
+        }
+
+        digits[idx] = 0;
+    }
+
+    //! All digits are nines at this point
+    digits.insert(digits.begin(), 1);
+    return digits;
+
+} // static std::vector<int> plusOneDS( ...
+
 TEST(PlusOneTest, SampleTest1)
 {
     const std::vector<int> expected_output {1, 2, 4};
 
     EXPECT_EQ(expected_output, plusOneFA({1, 2, 3}));
+    EXPECT_EQ(expected_output, plusOneDS({1, 2, 3}));
 }
 
 TEST(PlusOneTest, SampleTest2)
@@ -51,6 +84,7 @@ TEST(PlusOneTest, SampleTest2)
     const std::vector<int> expected_output {4, 3, 2, 2};
 
     EXPECT_EQ(expected_output, plusOneFA({4, 3, 2, 1}));
+    EXPECT_EQ(expected_output, plusOneDS({4, 3, 2, 1}));
 }
 
 TEST(PlusOneTest, SampleTest3)
@@ -58,4 +92,5 @@ TEST(PlusOneTest, SampleTest3)
     const std::vector<int> expected_output {9};
 
     EXPECT_EQ(expected_output, plusOneFA({1, 0}));
+    EXPECT_EQ(expected_output, plusOneDS({1, 0}));
 }
