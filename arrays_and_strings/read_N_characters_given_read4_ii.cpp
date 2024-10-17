fbfd07ce52a6d220ad9a27ee7d0f8223bd861ce5
Jeffry <jeffryklew@gmail.com>
2024-10-16 21:06:10 -0400

    WIP Implement first attempt skeleton in read_N_characters_given_read4_ii.cpp

---
+++ arrays_and_strings/read_N_characters_given_read4_ii.cpp
@@ -0,0 +1,14 @@
+#include <gtest/gtest.h>
+
+class SolutionFA
+{
+public:
+    //! First attempt to read n chars from file into buf
+    //! @param buf Destination buffer
+    //! @param n   Number of characters to read
+    //! @return The number of actual characters read
+    int read(char* buf, int n)
+    {
+        //! @todo
+    }
+};
