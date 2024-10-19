50d05631ecaa3566a829efb1229ffdefe913951d
Jeffry <jeffryklew@gmail.com>
2024-10-18 20:45:24 -0400

    WIP More changes to first attempt solution in read_N_characters_given_read4_ii.cpp

--- arrays_and_strings/read_N_characters_given_read4_ii.cpp
+++ arrays_and_strings/read_N_characters_given_read4_ii.cpp
@@ -1,45 +1,79 @@
 #include <gtest/gtest.h>
 
 #include <algorithm>
+#include <queue>
 
 class SolutionFA
 {
 public:
-    //! First attempt to read n chars from file into buf
-    //! @param buf Destination buffer
-    //! @param n   Number of characters to read
+    //! @brief First attempt to read n chars from file into buf
+    //! @param[out] buf Destination buffer
+    //! @param[in]  n   Number of characters to read
     //! @return The number of actual characters read
     int read(char* buf, int n)
     {
         //! Total number of chars read during this function call
         int total_chars_read {};
 
-        const int full_reads {n / 4};
+        //! If there are chars available in char_queue, write them to buf
+        while (!char_queue.empty() && total_chars_read < n)
+        {
+            *(buf + buf_offset) = char_queue.front();
+            char_queue.pop();
+            ++buf_offset;
+            ++total_chars_read;
+        }
 
-        for (int num_reads = 0; num_reads < full_reads; ++num_reads)
+        //! Read the requested number of chars
+        if (total_chars_read == n)
+        {
+            return total_chars_read;
+        }
+
+        //! Wrote all available chars in char_queue, need to read more
+        const int missing_chars {n - total_chars_read};
+
+        //! Read and copy chars to char_queue
+        int remaining_chars {missing_chars};
+        while (remaining_chars > 0)
         {
             const int curr_chars_read {read4(buf4)};
             if (curr_chars_read == 0)
             {
-                return total_chars_read;
+                //! No more chars available
+                break;
             }
 
-            std::copy(buf4, buf4 + curr_chars_read, buf + buf_offset);
-            total_chars_read += curr_chars_read;
-            buf_offset += curr_chars_read;
-        }
+            //! Copy chars from buf4 to char_queue
+            for (int buf4_idx = 0; buf4_idx < curr_chars_read; ++buf4_idx)
+            {
+                char_queue.push(buf4[buf4_idx]);
+            }
 
-        //! @todo Handle read for remaining chars
-        const int num_remaining_chars {n % 4};
-        const int num_read {read4(buf4)};
+            remaining_chars -= curr_chars_read;
+
+        } // while (remaining_chars > 0)
+
+        //! Write chars from char_queue
+        while (!char_queue.empty() && total_chars_read < missing_chars)
+        {
+            *(buf + buf_offset) = char_queue.front();
+            char_queue.pop();
+            ++buf_offset;
+            ++total_chars_read;
+        }
 
         return total_chars_read;
     }
 
 private:
     //! Position in buf to start writing at
-    int  buf_offset {};
+    int buf_offset {};
 
     //! Intermediate buffer to store result of read4
     char buf4[4] {};
-};
+
+    //! For storing chars across read4 calls
+    std::queue<char> char_queue {};
+
+}; // class SolutionFA
