2c8ea1899e6538c47986a280bf480a062dc44a4e
Jeffry <jeffryklew@gmail.com>
2024-10-28 20:21:16 -0400

    Implement sliding window discussion solution in longest_substring_with_at_most_two_distinct_characters.cpp

--- arrays_and_strings/longest_substring_with_at_most_two_distinct_characters.cpp
+++ arrays_and_strings/longest_substring_with_at_most_two_distinct_characters.cpp
@@ -1,8 +1,10 @@
 #include <gtest/gtest.h>
 
 #include <algorithm>
+#include <limits>
 #include <string>
 #include <unordered_map>
+#include <utility>
 
 //! @brief First attempt to get length of longest substring
 //! @param[in] s String to search substring in
@@ -61,23 +63,76 @@ static int lengthOfLongestSubstringTwoDistinctFA(std::string s)
 
 } // static int lengthOfLongestSubstringTwoDistinctFA( ...
 
+//! @brief Sliding window discussion solution
+//! @param[in] s String to search substring in
+//! @return Length of longest substring that contains at most two distinct chars
+static int lengthOfLongestSubstringTwoDistinctDS(std::string s)
+{
+    //! @details https://leetcode.com/problems
+    //!          /longest-substring-with-at-most-two-distinct-characters
+
+    const auto s_len = static_cast<int>(std::ssize(s));
+    if (s_len < 3)
+    {
+        return s_len;
+    }
+
+    int max_len {2};
+
+    //! Sliding window left pointer
+    int left {};
+
+    //! Map of <char, rightmost position in sliding window>
+    std::unordered_map<char, int> char_pos {};
+
+    for (int right = 0; right < s_len; ++right)
+    {
+        //! Sliding window contains less than 3 chars
+        char_pos[s[right]] = right++;
+
+        //! Sliding window contains 3 chars
+        if (std::ssize(char_pos) == 3)
+        {
+            int del_idx {std::numeric_limits<int>::max()};
+
+            for (const auto& char_idx : char_pos)
+            {
+                del_idx = std::min(del_idx, char_idx.second);
+            }
+
+            //! Delete leftmost character
+            char_pos.erase(s[del_idx]);
+            left = del_idx + 1;
+        }
+
+        max_len = std::max(max_len, right - left);
+    }
+
+    return max_len;
+
+} // static int lengthOfLongestSubstringTwoDistinctDS( ...
+
 TEST(LengthOfLongestSubstringTwoDistinctTest, SampleTest1)
 {
     EXPECT_EQ(3, lengthOfLongestSubstringTwoDistinctFA("eceba"));
+    EXPECT_EQ(3, lengthOfLongestSubstringTwoDistinctDS("eceba"));
 }
 
 TEST(LengthOfLongestSubstringTwoDistinctTest, SampleTest2)
 {
     EXPECT_EQ(5, lengthOfLongestSubstringTwoDistinctFA("ccaabbb"));
+    EXPECT_EQ(5, lengthOfLongestSubstringTwoDistinctDS("ccaabbb"));
 }
 
 TEST(LengthOfLongestSubstringTwoDistinctTest, SampleTest3)
 {
     EXPECT_EQ(1, lengthOfLongestSubstringTwoDistinctFA("a"));
+    EXPECT_EQ(1, lengthOfLongestSubstringTwoDistinctDS("a"));
 }
 
 TEST(LengthOfLongestSubstringTwoDistinctTest, SampleTest4)
 {
     EXPECT_NE(10, lengthOfLongestSubstringTwoDistinctFA("abccbbcccaaacaca"));
     EXPECT_EQ(14, lengthOfLongestSubstringTwoDistinctFA("abccbbcccaaacaca"));
+    EXPECT_EQ(10, lengthOfLongestSubstringTwoDistinctDS("abccbbcccaaacaca"));
 }
