b9423c828511dd33ce78ca850c2abf39977b9fb9
Jeffry <jeffryklew@gmail.com>
2024-10-31 21:30:04 -0400

    Finish first attempt solution in arrays_and_strings/missing_ranges.cpp

--- arrays_and_strings/missing_ranges.cpp
+++ arrays_and_strings/missing_ranges.cpp
@@ -10,20 +10,37 @@
 static std::vector<std::vector<int>>
     findMissingRangesFA(const std::vector<int>& nums, int lower, int upper)
 {
+    //! @details First attempt solution does not pass SampleTest6
+
     if (nums.empty())
     {
-        return {};
+        return {{lower, upper}};
     }
-
-    const auto nums_size = static_cast<int>(std::ssize(nums));
 
     std::vector<std::vector<int>> missing_ranges {};
 
-    int left {lower};
-
-    for (int right_idx = 0; right_idx < nums_size; ++right_idx)
+    if (nums.front() - lower != 0)
     {
-        //! @todo Continue
+        missing_ranges.push_back({lower, lower});
+    }
+
+    int prev {lower};
+
+    for (const int curr : nums)
+    {
+        if (curr - prev < 2)
+        {
+            prev = curr;
+            continue;
+        }
+
+        missing_ranges.push_back({prev + 1, curr - 1});
+        prev = curr;
+    }
+
+    if (upper - prev != 0)
+    {
+        missing_ranges.push_back({prev + 1, upper});
     }
 
     return missing_ranges;
@@ -49,4 +66,51 @@ TEST(FindMissingRangesTest, SampleTest2)
     constexpr int          upper {-1};
 
     EXPECT_TRUE(findMissingRangesFA(nums, lower, upper).empty());
+}
+
+TEST(FindMissingRangesTest, SampleTest3)
+{
+    const std::vector<int> nums {};
+    constexpr int          lower {1};
+    constexpr int          upper {1};
+
+    const std::vector<std::vector<int>> expected_output {{1, 1}};
+
+    EXPECT_EQ(expected_output, findMissingRangesFA(nums, lower, upper));
+}
+
+TEST(FindMissingRangesTest, SampleTest4)
+{
+    const std::vector<int> nums {-1};
+    constexpr int          lower {-2};
+    constexpr int          upper {-1};
+
+    const std::vector<std::vector<int>> expected_output {{-2, -2}};
+
+    EXPECT_EQ(expected_output, findMissingRangesFA(nums, lower, upper));
+}
+
+TEST(FindMissingRangesTest, SampleTest5)
+{
+    const std::vector<int> nums {-1};
+    constexpr int          lower {-1};
+    constexpr int          upper {};
+
+    const std::vector<std::vector<int>> expected_output {{0, 0}};
+
+    EXPECT_EQ(expected_output, findMissingRangesFA(nums, lower, upper));
+}
+
+TEST(FindMissingRangesTest, SampleTest6)
+{
+    const std::vector<int> nums {2};
+    constexpr int          lower {};
+    constexpr int          upper {9};
+
+    const std::vector<std::vector<int>> expected_output {{0, 1}, {3, 9}};
+    const std::vector<std::vector<int>> incorrect_output {
+        {0, 0}, {1, 1}, {3, 9}};
+
+    EXPECT_NE(expected_output, findMissingRangesFA(nums, lower, upper));
+    EXPECT_EQ(incorrect_output, findMissingRangesFA(nums, lower, upper));
 }
