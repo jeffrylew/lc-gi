bb8d2f90fe44a5b2f20cbfd31def9b382005add2
Jeffry <jeffryklew@gmail.com>
2024-11-05 20:18:36 -0500

    Implement binary search and fixed size sliding window discussion solution in longest_substring_with_at_most_k_distinct_characters.cpp

--- miscellaneous/longest_substring_with_at_most_k_distinct_characters.cpp
+++ miscellaneous/longest_substring_with_at_most_k_distinct_characters.cpp
@@ -2,6 +2,7 @@
 
 #include <algorithm>
 #include <string>
+#include <string_view>
 #include <unordered_map>
 
 //! @brief First attempt to get length of longest substring
@@ -67,6 +68,46 @@ static int lengthOfLongestSubstringKDistinctFA(std::string s, int k)
 
 } // static int lengthOfLongestSubstringKDistinctFA( ...
 
+//! @brief Helper function verifies if a valid substring of substr_size exists
+static bool is_valid_DS1(std::string_view s, int substr_size, int k)
+{
+    //! Count distinct chars in substring from [right - substr_size, right - 1]
+    std::unordered_map<char, int> char_count_in_substr {};
+
+    //! substr_size will be in [k, s.size()]
+    for (int idx = 0; idx < substr_size; ++idx)
+    {
+        ++char_count_in_substr[s[idx]];
+    }
+
+    if (std::ssize(char_count_in_substr) <= k)
+    {
+        return true;
+    }
+
+    //! Iterate right index from substr_size to s.size() - 1
+    for (int right = substr_size; right < std::ssize(s); ++right)
+    {
+        ++char_count_in_substr[s[right]];
+
+        const char prev_left_bound_char {s[right - substr_size]};
+        --char_count_in_substr[prev_left_bound_char];
+
+        if (char_count_in_substr[prev_left_bound_char] == 0)
+        {
+            char_count_in_substr.erase(prev_left_bound_char);
+        }
+
+        if (std::ssize(char_count_in_substr) <= k)
+        {
+            return true;
+        }
+    }
+
+    return false;
+
+} // static bool is_valid_DS1( ...
+
 //! @brief Binary Search + Fixed Size Sliding Window discussion solution
 //! @param[in] s std::string to search for longest substring in
 //! @param[in] k Number of distinct chars at most that substring can have
@@ -75,8 +116,38 @@ static int lengthOfLongestSubstringKDistinctDS1(std::string s, int k)
 {
     //! @details https://leetcode.com/problems
     //!          /longest-substring-with-at-most-k-distinct-characters/editorial
+    //!
+    //!          Time complexity O(N * log N) where N = s.size(). Set the search
+    //!          space as [k, N] so it takes at most O(log N) for binary search.
+    //!          At each step, iterate over s which takes O(N).
+    //!          Space complexity O(N). Use hash map char_count_in_substr which
+    //!          contains at most O(N) distinct chars.
 
-    //! @todo
+    const auto s_len = std::ssize(s);
+    if (k >= s_len)
+    {
+        return s_len;
+    }
+
+    int min_substr_size {k};
+    int max_substr_size {s_len};
+
+    while (min_substr_size < max_substr_size)
+    {
+        const int mid_substr_size {
+            min_substr_size + (max_substr_size - min_substr_size + 1) / 2};
+
+        if (is_valid_DS1(s, mid_substr_size, k))
+        {
+            min_substr_size = mid_substr_size;
+        }
+        else
+        {
+            max_substr_size = mid_substr_size - 1;
+        }
+    }
+
+    return min_substr_size;
 
 } // static int lengthOfLongestSubstringKDistinctDS1( ...
 
