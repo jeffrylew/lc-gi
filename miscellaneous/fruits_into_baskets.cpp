c8b9e1b24ede7b25923b9606b25e59ea56b6bc21
Jeffry <jeffryklew@gmail.com>
2024-11-10 11:26:18 -0500

    Implement sliding window discussion solution in miscellaneous/fruits_into_baskets.cpp

--- miscellaneous/fruits_into_baskets.cpp
+++ miscellaneous/fruits_into_baskets.cpp
@@ -43,17 +43,71 @@ static int totalFruitFA(std::vector<int> fruits)
 
 } // static int totalFruitFA( ...
 
+//! Skip Brute Force (DS1) and Optimized Brute Force (DS2) discussion solutions
+
+//! @brief Sliding window discussion solution to get max number of fruits
+//! @param[in] fruits Vector of fruits where fruits[i] is the type of fruit at i
+//! @return Max number of fruits if you have two baskets for two types of fruit
+static int totalFruitDS3(std::vector<int> fruits)
+{
+    //! @details https://leetcode.com/problems/fruit-into-baskets/editorial/
+    //!
+    //!          Time complexity O(N) where N = fruits.size(). left and right
+    //!          monotonically increase so we have at most 2 * N steps. At each
+    //!          step, we update the hash map by adding and deleting fruits
+    //!          which take constant time.
+    //!          Space complexity O(N). In the worst case scenario, there might
+    //!          be O(N) types of fruits in the window. For example, if we have
+    //           fruits = [1, 1, 1, ..., 1, 2, 3, 4, ..., N/2 + 1] then
+    //!          fruit_count.size() == 1 for fruit type = 1. In the second half
+    //!          of the vector, fruit_count.size() grows to N / 2.
+
+    //! Map of <fruit type, count in window of trees>
+    std::unordered_map<int, int> fruit_count {};
+
+    int left {};
+    int right {};
+
+    while (right < std::ssize(fruits))
+    {
+        //! Add fruit from the right index
+        ++fruit_count[fruits[right]];
+
+        //! If current window has more than two types of fruit,
+        //! remove one fruit from the left index of the window
+        if (std::ssize(fruit_count) > 2)
+        {
+            if (--fruit_count[fruits[left]] == 0)
+            {
+                fruit_count.erase(fruits[left]);
+            }
+
+            ++left;
+        }
+
+        ++right;
+    }
+
+    //! Indices left and right will contain the longest valid subarray
+    //! right == fruits.size() after the while loop
+    return right - left;
+
+} // static int totalFruitDS3( ...
+
 TEST(TotalFruitTest, SampleTest1)
 {
     EXPECT_EQ(3, totalFruitFA({1, 2, 1}));
+    EXPECT_EQ(3, totalFruitDS3({1, 2, 1}));
 }
 
 TEST(TotalFruitTest, SampleTest2)
 {
     EXPECT_EQ(3, totalFruitFA({0, 1, 2, 2}));
+    EXPECT_EQ(3, totalFruitDS3({0, 1, 2, 2}));
 }
 
 TEST(TotalFruitTest, SampleTest3)
 {
     EXPECT_EQ(4, totalFruitFA({1, 2, 3, 2, 2}));
+    EXPECT_EQ(4, totalFruitDS3({1, 2, 3, 2, 2}));
 }
