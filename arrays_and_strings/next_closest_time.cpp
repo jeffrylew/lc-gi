e0325b156b7ed6ee96063cdb77a688ec954b2b62
Jeffry <jeffryklew@gmail.com>
2024-11-15 16:19:42 -0500

    Implement simulation discussion solution in arrays_and_strings/next_closest_time.cpp

--- arrays_and_strings/next_closest_time.cpp
+++ arrays_and_strings/next_closest_time.cpp
@@ -1,7 +1,11 @@
 #include <gtest/gtest.h>
 
+#include <algorithm>
+#include <array>
+#include <format>
 #include <set>
 #include <string>
+#include <unordered_set>
 
 //! @brief First attempt to form next closest time by reusing current digits
 //! @param[in] time std::string containing a time in the format "HH:MM"
@@ -66,23 +70,86 @@ static std::string nextClosestTimeFA(std::string time)
 
 } // static std::string nextClosestTimeFA( ...
 
+//! @brief Simulation discussion solution
+//! @param[in] time std::string containing a time in the format "HH:MM"
+//! @return st::string containing the next closest time by reusing digits
+static std::string nextClosestTimeDS1(std::string time)
+{
+    //! @details https://leetcode.com/problems/next-closest-time/editorial/
+    //!
+    //!          Time complexity O(1). We try up to 24 * 60 possible times until
+    //!          we find the correct time.
+    //!          Space complexity O(1). allowed_digits set and curr_digits array
+    //!          both store 4 elements max.
+
+    int curr_time_in_minutes {
+        std::stoi(time.substr(0, 2)) * 60 + std::stoi(time.substr(3))};
+
+    std::unordered_set<int> allowed_digits {};
+
+    for (const char ch : time)
+    {
+        if (ch == ':')
+        {
+            continue;
+        }
+
+        allowed_digits.insert(static_cast<int>(ch - '0'));
+    }
+
+    std::array<int, 4> curr_digits {};
+
+    while (true)
+    {
+        curr_time_in_minutes = (curr_time_in_minutes + 1) % (24 * 60);
+
+        const int curr_hours {curr_time_in_minutes / 60};
+        const int curr_mins {curr_time_in_minutes % 60};
+
+        curr_digits[0] = curr_hours / 10;
+        curr_digits[1] = curr_hours % 10;
+        curr_digits[2] = curr_mins / 10;
+        curr_digits[3] = curr_mins % 10;
+
+        if (std::all_of(curr_digits.begin(),
+                        curr_digits.end(),
+                        [&](int digit) {
+                            return allowed_digits.contains(digit);
+                        }))
+        {
+            break;
+        }
+    }
+
+    return std::format("{}{}:{}{}",
+                       curr_digits[0],
+                       curr_digits[1],
+                       curr_digits[2],
+                       curr_digits[3]);
+
+} // static std::string nextClosestTimeDS1( ...
+
 TEST(NextClosestTimeTest, SampleTest1)
 {
     EXPECT_EQ("19:39", nextClosestTimeFA("19:34"));
+    EXPECT_EQ("19:39", nextClosestTimeDS1("19:34"));
 }
 
 TEST(NextClosestTimeTest, SampleTest2)
 {
     EXPECT_EQ("22:22", nextClosestTimeFA("23:59"));
+    EXPECT_EQ("22:22", nextClosestTimeDS1("23:59"));
 }
 
 TEST(NextClosestTimeTest, SampleTest3)
 {
     EXPECT_EQ("15:11", nextClosestTimeFA("13:55"));
+    EXPECT_EQ("15:11", nextClosestTimeDS1("13:55"));
 }
 
 TEST(NextClosestTimeTest, SampleTest4)
 {
     EXPECT_NE("22:32", nextClosestTimeFA("22:23"));
     EXPECT_EQ("22:22", nextClosestTimeFA("22:23"));
+    EXPECT_EQ("22:32", nextClosestTimeDS1("22:23"));
 }
