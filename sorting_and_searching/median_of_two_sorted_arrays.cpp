27bef328fe31c7c10335e1d82f03dc5741a9fcbb
Jeffry <jeffryklew@gmail.com>
2024-11-25 20:53:24 -0500

    Implement merge sort discussion solution in sorting_and_searching/median_of_two_sorted_arrays.cpp

--- sorting_and_searching/median_of_two_sorted_arrays.cpp
+++ sorting_and_searching/median_of_two_sorted_arrays.cpp
@@ -81,12 +81,72 @@ static double findMedianSortedArraysFA(std::vector<int> nums1,
 
 } // static double findMedianSortedArraysFA( ...
 
+//! @brief Merge sort discussion solution
+//! @param[in] nums1 Sorted vector of size m
+//! @param[in] nums2 Sorted vector of size n
+//! @return Median of sorted vectors in O(log (m + n))
+static double findMedianSortedArraysDS1(std::vector<int> nums1,
+                                        std::vector<int> nums2)
+{
+    //! @details leetcode.com/problems/median-of-two-sorted-arrays/editorial
+    //!
+    //!          Time complexity O(m + n) where m = nums1.size() and
+    //!          n = nums2.size(). Get smallest element from values at idx1 and
+    //!          idx2. Need to traverse half of the vectors before reaching the
+    //!          median element(s).
+    //!          Space complexity O(1) for two pointers idx1 and idx2.
+
+    const auto nums1_len = static_cast<int>(std::ssize(nums1));
+    const auto nums2_len = static_cast<int>(std::ssize(nums2));
+
+    int idx1 {};
+    int idx2 {};
+
+    //! Get smaller value between nums1[idx1] and nums2[idx2] and increment idx
+    const auto get_min = [&] {
+        if (idx1 < nums1_len && idx2 < nums2_len)
+        {
+            return nums1[idx1] < nums2[idx2] ? nums1[idx1++] : nums2[idx2++];
+        }
+        else if (idx1 < nums1_len)
+        {
+            return nums1[idx1++];
+        }
+        else if (idx2 < nums2_len)
+        {
+            return nums2[idx2++];
+        }
+
+        return -1;
+    };
+
+    if ((nums1_len + nums2_len) % 2 == 0)
+    {
+        for (int idx = 0; idx < (nums1_len + nums2_len) / 2 - 1; ++idx)
+        {
+            get_min();
+        }
+
+        return static_cast<double>(get_min() + get_min()) / 2.0;
+    }
+
+    for (int idx = 0; idx < (nums1_len + nums2_len) / 2; ++idx)
+    {
+        get_min();
+    }
+
+    return get_min();
+
+} // static double findMedianSortedArraysDS1( ...
+
 TEST(FindMedianSortedArraysTest, SampleTest1)
 {
     EXPECT_EQ(2.0, findMedianSortedArraysFA({1, 3}, {2}));
+    EXPECT_EQ(2.0, findMedianSortedArraysDS1({1, 3}, {2}));
 }
 
 TEST(FindMedianSortedArraysTest, SampleTest2)
 {
     EXPECT_EQ(2.5, findMedianSortedArraysFA({1, 2}, {3, 4}));
+    EXPECT_EQ(2.5, findMedianSortedArraysDS1({1, 2}, {3, 4}));
 }
