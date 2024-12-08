24926accb37c40379cf840da5fa545f1b2faaa15
Jeffry <jeffryklew@gmail.com>
2024-12-08 14:14:46 -0500

    Implement vertical scanning discussion solution in longest_common_prefix.cpp

--- miscellaneous/longest_common_prefix.cpp
+++ miscellaneous/longest_common_prefix.cpp
@@ -53,7 +53,8 @@ static std::string longestCommonPrefixDS1(std::vector<std::string> strs)
     //!          In the worst case, all N strings are the same. The algorithm
     //!          compares S_1 with other strings [S_2, S_N]. There are S char
     //!          comparisons.
-    //!          Space complexity O(1). Only use constant extra space.
+    //!          Space complexity O(1). Only use constant extra space, excluding
+    //!          the space for the output prefix.
 
     if (strs.empty())
     {
@@ -78,14 +79,55 @@ static std::string longestCommonPrefixDS1(std::vector<std::string> strs)
 
 } // static std::string longestCommonPrefixDS1( ...
 
+//! @brief Vertical scanning discussion solution
+//! @param[in] strs Vector of strings to search for longest common prefix in
+//! @return Longest common prefix. If none, return empty string.
+static std::string longestCommonPrefixDS2(std::vector<std::string> strs)
+{
+    //! @details https://leetcode.com/problems/longest-common-prefix/editorial/
+    //!
+    //!          Time complexity O(S) where S = sum of all chars in all strings.
+    //!          In the worst case, there will be N equal strings of length M
+    //!          and the algorithm performs S = M * N character comparisons. The
+    //!          worst case is the same as in DS1 but in the best case there are
+    //!          at most N * min_len comparisons, where min_len is the length of
+    //!          the shortest string in the vector.
+    //!          Space complexity O(1). Only use constant extra space, excluding
+    //!          the space for the output prefix.
+
+    if (strs.empty())
+    {
+        return {};
+    }
+
+    for (int strs0_idx = 0; strs0_idx < std::ssize(strs[0]); ++strs0_idx)
+    {
+        const char strs0_char {strs[0][strs0_idx]};
+
+        for (int strs_idx = 1; strs_idx < std::ssize(strs); ++strs_idx)
+        {
+            if (strs0_idx == std::ssize(strs[strs_idx])
+                || strs[strs_idx][strs0_idx] != strs0_char)
+            {
+                return strs[0].substr(0, strs0_idx);
+            }
+        }
+    }
+
+    return strs[0];
+
+} // static std::string longestCommonPrefixDS2( ...
+
 TEST(LongestCommonPrefixTest, SampleTest1)
 {
     EXPECT_EQ("fl", longestCommonPrefixFA({"flower", "flow", "flight"}));
     EXPECT_EQ("fl", longestCommonPrefixDS1({"flower", "flow", "flight"}));
+    EXPECT_EQ("fl", longestCommonPrefixDS2({"flower", "flow", "flight"}));
 }
 
 TEST(LongestCommonPrefixTest, SampleTest2)
 {
     EXPECT_TRUE(longestCommonPrefixFA({"dog", "racecar", "car"}).empty());
     EXPECT_TRUE(longestCommonPrefixDS1({"dog", "racecar", "car"}).empty());
+    EXPECT_TRUE(longestCommonPrefixDS2({"dog", "racecar", "car"}).empty());
 }
