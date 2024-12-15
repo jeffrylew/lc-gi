ef49b231efb060347d388f97b03f2ea770e66a08
Jeffry <jeffryklew@gmail.com>
2024-12-15 12:22:58 -0500

    Implement first attempt solution in find_and_replace_in_string.cpp

--- arrays_and_strings/find_and_replace_in_string.cpp
+++ arrays_and_strings/find_and_replace_in_string.cpp
@@ -1,14 +1,57 @@
 #include <gtest/gtest.h>
 
 #include <string>
+#include <string_view>
 #include <vector>
 
+//! @brief 1st attempt to replace sources[i] with targets[i] in s at indices[i]
+//! @param[in] s       Original std::string to perform replacements in
+//! @param[in] indices Vector of indices in the original s to replace at
+//! @param[in] sources Vector of substrings that must exist in s at indices[i]
+//! @param[in] targets Vector of substrings to replace with in s at indices[i]
 static std::string findReplaceStringFA(std::string              s,
                                        std::vector<int>         indices,
                                        std::vector<std::string> sources,
                                        std::vector<std::string> targets)
 {
-    //! @todo
+    //! @details https://leetcode.com/explore/interview/card/google/59
+    //!          /array-and-strings/3057/
+    //!
+    //!          First attempt does not pass SampleTest3
+
+    std::string replacement {};
+    const auto  indices_size = static_cast<int>(std::ssize(indices));
+
+    int                    s_idx {};
+    const std::string_view s_view {s};
+
+    for (int idx = 0; idx < indices_size; ++idx)
+    {
+        const int              curr_idx {indices[idx]};
+        const std::string_view curr_source {sources[idx]};
+        const std::string_view curr_target {targets[idx]};
+
+        const auto curr_source_size = static_cast<int>(std::ssize(curr_source));
+        const auto curr_target_size = static_cast<int>(std::ssize(curr_target));
+
+        while (s_idx < curr_idx)
+        {
+            replacement += s_view[s_idx++];
+        }
+
+        if (s_view.substr(curr_idx, curr_source_size) == curr_source)
+        {
+            replacement.append(curr_target);
+            s_idx = curr_idx + curr_source_size;
+        }
+        else
+        {
+            replacement.append(s_view.substr(s_idx, curr_target_size));
+            s_idx = curr_idx + curr_target_size;
+        }
+    }
+
+    return replacement;
 
 } // static std::string findReplaceStringFA( ...
 
@@ -26,4 +69,15 @@ TEST(FindReplaceStringTest, SampleTest2)
                                            {0, 2},
                                            {"ab", "ec"},
                                            {"eee", "ffff"}));
+}
+
+TEST(FindReplaceStringTest, SampleTest3)
+{
+    const std::string      s {"vmokgggqzp"};
+    const std::vector<int> indices {3, 5, 1};
+    const std::vector<std::string> sources {"kg", "ggq", "mo"};
+    const std::vector<std::string> targets {"s", "so", "bfr"};
+
+    EXPECT_EQ("vmossobfr", findReplaceStringFA(s, indices, sources, targets));
+    EXPECT_NE("vbfrssozp", findReplaceStringFA(s, indices, sources, targets));
 }
