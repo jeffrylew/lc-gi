97705c23440c1df5367c686854bbee56b82527e0
Jeffry <jeffryklew@gmail.com>
2024-12-26 13:01:21 -0500

    Implement discussion solution in find_first_and_last_position_of_element_in_sorted_array.cpp

--- sorting_and_searching/find_first_and_last_position_of_element_in_sorted_array.cpp
+++ sorting_and_searching/find_first_and_last_position_of_element_in_sorted_array.cpp
@@ -64,11 +64,75 @@ static std::vector<int> searchRangeFA(std::vector<int> nums, int target)
 
 } // static std::vector<int> searchRangeFA( ...
 
+//! @brief Discussion solution to find start and end position of target value
+//! @param[in] nums   Vector of sorted ints in non-decreasing order
+//! @param[in] target Value to find start and end position of
+//! @return Vector containing start and end position of target, or [-1, -1]
+static std::vector<int> searchRangeDS(std::vector<int> nums, int target)
+{
+    //! @details https://leetcode.com/problems
+    //!          /find-first-and-last-position-of-element-in-sorted-array
+    //!
+    //!          Time complexity O(log N) where N = nums.size()
+    //!          Space complexity O(1)
+
+    const auto find_bound = [&](bool searching_left) -> int {
+        int bound_index {-1};
+
+        int left {};
+        int right {static_cast<int>(std::ssize(nums)) - 1};
+
+        while (left <= right)
+        {
+            const int mid {left + (right - left) / 2};
+
+            if (nums[mid] == target)
+            {
+                bound_index = mid;
+
+                if (searching_left)
+                {
+                    right = mid - 1;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+            else if (nums[mid] > target)
+            {
+                right = mid - 1;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+
+        return bound_index;
+    };
+
+    if (nums.empty())
+    {
+        return {-1, -1};
+    }
+
+    const int left_idx {find_bound(true)};
+    if (left_idx == -1)
+    {
+        return {-1, -1};
+    }
+
+    return {left_idx, find_bound(false)};
+
+} // static std::vector<int> searchRangeDS( ...
+
 TEST(SearchRangeTest, SampleTest1)
 {
     const std::vector<int> expected_output {3, 4};
 
     EXPECT_EQ(expected_output, searchRangeFA({5, 7, 7, 8, 8, 10}, 8));
+    EXPECT_EQ(expected_output, searchRangeDS({5, 7, 7, 8, 8, 10}, 8));
 }
 
 TEST(SearchRangeTest, SampleTest2)
@@ -76,6 +140,7 @@ TEST(SearchRangeTest, SampleTest2)
     const std::vector<int> expected_output {-1, -1};
 
     EXPECT_EQ(expected_output, searchRangeFA({5, 7, 7, 8, 8, 10}, 6));
+    EXPECT_EQ(expected_output, searchRangeDS({5, 7, 7, 8, 8, 10}, 6));
 }
 
 TEST(SearchRangeTest, SampleTest3)
@@ -83,4 +148,13 @@ TEST(SearchRangeTest, SampleTest3)
     const std::vector<int> expected_output {-1, -1};
 
     EXPECT_EQ(expected_output, searchRangeFA({}, 0));
+    EXPECT_EQ(expected_output, searchRangeDS({}, 0));
+}
+
+TEST(SearchRangeTest, SampleTest4)
+{
+    const std::vector<int> expected_output {-1, -1};
+
+    EXPECT_EQ(expected_output, searchRangeFA({2, 2}, 3));
+    EXPECT_EQ(expected_output, searchRangeDS({2, 2}, 3));
 }
