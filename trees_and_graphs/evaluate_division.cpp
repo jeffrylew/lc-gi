18662fa928a920dbfcdf83e831e79213dd94c01c
Jeffry <jeffryklew@gmail.com>
2024-12-29 17:11:57 -0500

    Implement path search discussion solution in trees_and_graphs/evaluate_division.cpp

--- trees_and_graphs/evaluate_division.cpp
+++ trees_and_graphs/evaluate_division.cpp
@@ -53,7 +53,7 @@ static double answerQueryDS1(std::string_view start,
 
 } // static double answerQueryDS1( ...
 
-//! @brief Path search in graph discussion soln to get answers to all queries
+//! @brief First discussion solution to get answers to all queries
 //! @param[in] equations Reference to vector of variable pairs [A_i, B_i]
 //! @param[in] values    Reference to vector of values A_i / B_i = values[i]
 //! @param[in] queries   Reference to vector of queries [C_j, D_j]
@@ -105,6 +105,109 @@ static std::vector<double> calcEquationDS1(
 
 } // static std::vector<double> calcEquationFA( ...
 
+static double backtrack_evaluate_DS2(
+    const Graph_t&                        graph,
+    std::string_view                      curr_node,
+    std::string_view                      target_node,
+    double                                acc_product,
+    std::unordered_set<std::string_view>& visited)
+{
+    //! Mark the visit
+    visited.insert(curr_node);
+
+    double answer {-1.0};
+
+    const auto& neighors = graph[curr_node];
+    if (neighors.contains(target_node))
+    {
+        answer = acc_product * neighbors[target_node];
+    }
+    else
+    {
+        for (const auto& [next_node, next_val] : neighbors)
+        {
+            if (visited.contains(next_node))
+            {
+                continue;
+            }
+
+            answer = backtrack_evaluate_DS2(graph,
+                                            next_node,
+                                            target_node,
+                                            acc_product * next_val,
+                                            visited);
+
+            if (answer != -1.0)
+            {
+                break;
+            }
+        }
+    }
+
+    //! Unmark the visit for the next backtracking
+    visited.erase(curr_node);
+    return answer;
+
+} // static double backtrack_evaluate_DS2( ...
+
+//! @brief Path search in graph discussion soln to get answers to all queries
+//! @param[in] equations Reference to vector of variable pairs [A_i, B_i]
+//! @param[in] values    Reference to vector of values A_i / B_i = values[i]
+//! @param[in] queries   Reference to vector of queries [C_j, D_j]
+//! @return Answers to all queries. If cannot be determined, return -1.0
+static std::vector<double> calcEquationDS2(
+    const std::vector<std::vector<std::string>>& equations,
+    const std::vector<double>&                   values,
+    const std::vector<std::vector<std::string>>& queries)
+{
+    //! @details https://leetcode.com/problems/evaluate-division/editorial/
+
+    Graph_t graph {};
+
+    //! Step 1) Build graph from the equations
+    for (int idx = 0; idx < std::ssize(equations); ++idx)
+    {
+        const auto&  dividend = equations[idx].front();
+        const auto&  divisor  = equations[idx].back();
+        const double quotient {values[idx]};
+
+        graph[dividend][divisor] = quotient;
+        graph[divisor][dividend] = 1 / quotient;
+    }
+
+    //! Step 2) Evaluate each query via backtracking (DFS) by verifying
+    //! whether there exists a path from dividend to divisor
+    std::vector<double> answers {};
+    answers.reserve(queries.size());
+
+    for (int idx = 0; idx < std::ssize(queries); ++idx)
+    {
+        const auto& dividend = queries[idx].front();
+        const auto& divisor  = queries[idx].back();
+
+        if (!graph.contains(dividend) || !graph.contains(divisor))
+        {
+            answers.push_back(-1.0);
+        }
+        else if (dividend == divisor)
+        {
+            answers.push_back(-1.0);
+        }
+        else
+        {
+            std::unordered_set<std::string_view> visited {};
+            answers.push_back(backtrack_evaluate_DS2(graph,
+                                                     dividend,
+                                                     divisor,
+                                                     1.0,
+                                                     visited));
+        }
+    }
+
+    return results;
+
+} // static std::vector<double> calcEquationDS2( ...
+
 TEST(CalcEquationTest, SampleTest1)
 {
     const std::vector<std::vector<std::string>> equations {
@@ -115,6 +218,7 @@ TEST(CalcEquationTest, SampleTest1)
     const std::vector<double> expected_output {6.0, 0.5, -1.0, 1.0, -1.0};
 
     EXPECT_EQ(expected_output, calcEquationDS1(equations, values, queries));
+    EXPECT_EQ(expected_output, calcEquationDS2(equations, values, queries));
 }
 
 TEST(CalcEquationTest, SampleTest2)
@@ -127,6 +231,7 @@ TEST(CalcEquationTest, SampleTest2)
     const std::vector<double> expected_output {3.75, 0.4, 5.0, 0.2};
 
     EXPECT_EQ(expected_output, calcEquationDS1(equations, values, queries));
+    EXPECT_EQ(expected_output, calcEquationDS2(equations, values, queries));
 }
 
 TEST(CalcEquationTest, SampleTest3)
@@ -138,4 +243,5 @@ TEST(CalcEquationTest, SampleTest3)
     const std::vector<double> expected_output {0.5, 2.0, -1.0, -1.0};
 
     EXPECT_EQ(expected_output, calcEquationDS1(equations, values, queries));
+    EXPECT_EQ(expected_output, calcEquationDS2(equations, values, queries));
 }
