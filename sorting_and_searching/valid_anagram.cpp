1d968296e1039831b1e9d84415e6208ea7471f05
Jeffry <jeffryklew@gmail.com>
2025-01-15 19:34:54 -0500

    Implement frequency counter discussion solution in sorting_and_searching/valid_anagram.cpp

--- sorting_and_searching/valid_anagram.cpp
+++ sorting_and_searching/valid_anagram.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 
 #include <algorithm>
+#include <array>
 #include <string>
 #include <unordered_map>
 
@@ -64,14 +65,54 @@ static bool isAnagramDS1(std::string s, std::string t)
 
 } // static bool isAnagramDS1( ...
 
+//! @brief Frequency counter discussion solution
+//! @param[in] s First string
+//! @param[in] t Second string to check if is an anagram
+//! @return True if t is an anagram of s, else false
+static bool isAnagramDS2(std::string s, std::string t)
+{
+    //! @details https://leetcode.com/problems/valid-anagram/editorial/
+    //!
+    //!          Time complexity O(N) because accessing char_count is a constant
+    //!          time operation.
+    //!          Space complexity O(1) since the size of char_count is fixed.
+
+    if (std::ssize(s) != std::ssize(t))
+    {
+        return false;
+    }
+
+    std::array<int, 26> char_count {};
+    for (const char ch : s)
+    {
+        ++char_count[static_cast<std::size_t>(ch - 'a')];
+    }
+
+    for (const char ch : t)
+    {
+        const auto ch_idx = static_cast<std::size_t>(ch - 'a');
+
+        --char_count[ch_idx];
+        if (char_count[ch_idx] < 0)
+        {
+            return false;
+        }
+    }
+
+    return true;
+
+} // static bool isAnagramDS2( ...
+
 TEST(IsAnagramTest, SampleTest1)
 {
     EXPECT_TRUE(isAnagramFA("anagram", "nagaram"));
     EXPECT_TRUE(isAnagramDS1("anagram", "nagaram"));
+    EXPECT_TRUE(isAnagramDS2("anagram", "nagaram"));
 }
 
 TEST(IsAnagramTest, SampleTest2)
 {
     EXPECT_FALSE(isAnagramFA("rat", "car"));
     EXPECT_FALSE(isAnagramDS1("rat", "car"));
+    EXPECT_FALSE(isAnagramDS2("rat", "car"));
 }
