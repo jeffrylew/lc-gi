90c21dfdd2de567c5920f6602e4e8f271b04aefb
Jeffry <jeffryklew@gmail.com>
2025-01-18 22:40:56 -0500

    WIP Implement first attempt skeleton in robot_room_cleaner.cpp

---
+++ trees_and_graphs/robot_room_cleaner.cpp
@@ -0,0 +1,27 @@
+#include <gtest/gtest.h>
+
+/*
+ * This is the robot's control interface
+ * You should not implement it or speculate about its implementation
+ *
+class Robot
+{
+public:
+    // Returns true if the cell in front is open and robot moves into the cell.
+    // Returns false if front cell is blocked and robot stays in current cell
+    bool move();
+
+    // Robot will stay in the same cell after calling turnLeft/turnRight
+    // Each turn will be 90 degrees
+    void turnLeft();
+    void turnRight();
+
+    // Clean the current cell
+    void clean();
+};
+ */
+
+void cleanRoomFA(Robot& robot)
+{
+
+}
