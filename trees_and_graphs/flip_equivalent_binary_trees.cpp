31a6eb085b1b1cc8bcb875c777292f0720efb567
Jeffry <jeffryklew@gmail.com>
2025-02-02 13:39:22 -0500

    Implement recursive top-down traversal discussion solution in flip_equivalent_binary_trees.cpp

--- trees_and_graphs/flip_equivalent_binary_trees.cpp
+++ trees_and_graphs/flip_equivalent_binary_trees.cpp
@@ -98,6 +98,56 @@ static bool flipEquivFA(TreeNode* root1, TreeNode* root2)
 
 } // static bool flipEquivFA( ...
 
+//! @brief Recursive (top-down traversal) discussion solution
+//! @param[in, out] root1 Pointer to root of first binary tree
+//! @param[in, out] root2 Pointer to root of second binary tree
+//! @return True if two trees are flip equivalent, else false
+static bool flipEquivDS1(TreeNode* root1, TreeNode* root2)
+{
+    //! @details https://leetcode.com/problems/flip-equivalent-binary-trees
+    //!
+    //!          Time complexity O(N) where N = number of nodes in the smaller
+    //!          tree. Recursion stops at the leaf nodes or when a mismatch
+    //!          occurs. In the worst case, every node in the smaller tree will
+    //!          be visited.
+    //!          Space complexity O(N) due to the recursion stack. In the worst
+    //!          case, the recursion is deep as the height of the tree, which is
+    //!          O(N) in the case of a skewed tree (each node has one child).
+    //!          For a balanced tree, the space complexity is O(log N) because
+    //!          the height is logarithmic relative to the number of nodes.
+
+    //! Both trees are empty
+    if (root1 == nullptr && root2 == nullptr)
+    {
+        return true;
+    }
+
+    //! One of the trees is empty
+    if (root1 == nullptr || root2 == nullptr)
+    {
+        return false;
+    }
+
+    //! Corresponding values differ
+    if (root1->val != root2->val)
+    {
+        return false;
+    }
+
+    //! Check if corresponding subtrees are flip equivalent
+    const bool flip_equivalent_with_no_swaps {
+        flipEquivDS1(root1->left, root2->left)
+        && flipEquivDS1(root1->right, root2->right)};
+
+    //! Check if opposite subtrees are flip equivalent
+    const bool flip_equivalent_with_swaps {
+        flipEquivDS1(root1->left, root2->right)
+        && flipEquivDS1(root1->right, root2->left)};
+
+    return flip_equivalent_with_no_swaps || flip_equivalent_with_swaps;
+
+} // static bool flipEquivDS1( ...
+
 TEST(FlipEquivTest, SampleTest1)
 {
     const TreeNode root1_four {4};
@@ -121,11 +171,13 @@ TEST(FlipEquivTest, SampleTest1)
     TreeNode root2_one {1, &root2_three, &root2_two};
 
     EXPECT_TRUE(flipEquivFA(&root1_one, &root2_one));
+    EXPECT_TRUE(flipEquivDS1(&root1_one, &root2_one));
 }
 
 TEST(FlipEquivTest, SampleTest2)
 {
     EXPECT_TRUE(flipEquivFA(nullptr, nullptr));
+    EXPECT_TRUE(flipEquivDS1(nullptr, nullptr));
 }
 
 TEST(FlipEquivTest, SampleTest3)
@@ -133,6 +185,7 @@ TEST(FlipEquivTest, SampleTest3)
     const TreeNode one {1};
 
     EXPECT_FALSE(flipEquivFA(nullptr, &one));
+    EXPECT_FALSE(flipEquivDS1(nullptr, &one));
 }
 
 TEST(FlipEquivTest, SampleTest4)
@@ -146,4 +199,5 @@ TEST(FlipEquivTest, SampleTest4)
     TreeNode       root2_one {1, &root2_two, nullptr};
 
     EXPECT_FALSE(flipEquivFA(&root1_one, &root2_one));
+    EXPECT_FALSE(flipEquivDS1(&root1_one, &root2_one));
 }
