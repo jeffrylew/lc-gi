94c395207f629397533e47838501352412aa533b
Jeffry <jeffryklew@gmail.com>
2025-02-15 13:41:34 -0500

    Implement first attempt solution in strobogrammatic_number_ii.cpp

--- recursion/strobogrammatic_number_ii.cpp
+++ recursion/strobogrammatic_number_ii.cpp
@@ -1,21 +1,56 @@
 #include <gtest/gtest.h>
 
 #include <string>
+#include <unordered_map>
+#include <utility>
 #include <vector>
 
-static void find_strobogrammatic_nums(std::vector<std::string>& s_nums,
-                                      std::string&              s_num,
-                                      int                       s_num_len)
+//! @brief Recursive helper to find strobogrammatic numbers of length curr_len
+//! @param[in, out] snum_map Reference to map to memoize nums at varying lengths
+//! @param[in]      curr_len Current length of strobogrammatic numbers to find
+//! @return Reference to vector of strobogrammatic numbers with length curr_len
+static const std::vector<std::string>& find_strobogrammatic_nums(
+    std::unordered_map<int, std::vector<std::string>>& snum_map, int curr_len)
 {
-    if (std::ssize(s_num) == s_num_len)
+    if (snum_map.contains(curr_len))
     {
-        s_nums.push_back(s_num);
-        return;
+        return snum_map[curr_len];
     }
 
-    //! @todo Strobogrammatic number like bracket matching or anagram. Maybe use
-    //!       stack while backtracking to get correct order.
-}
+    //! Get vector of strobogrammatic numbers with length curr_len - 2
+    const auto& prev_snums = find_strobogrammatic_nums(snum_map, curr_len - 2);
+
+    //! Reserve space for new strobogrammatic numbers. Multiply by 4 to account
+    //! for inserting 1/6/8/9. ssize(prev_snums) = ssize(snum_map[curr_len - 2])
+    std::vector<std::string> new_snums;
+    new_snums.reserve(4U * prev_snums.size());
+
+    //! New strobogrammatic numbers cannot begin with zero
+    for (const char num_to_insert : {'1', '6', '8', '9'})
+    {
+        for (const auto& prev_snum : prev_snums)
+        {
+            auto& new_snum = new_snums.emplace_back(1U, num_to_insert);
+            new_snum += prev_snum;
+
+            switch (num_to_insert)
+            {
+                case '6':
+                    new_snum += '9';
+                    break;
+                case '9':
+                    new_snum += '6';
+                    break;
+                default:
+                    new_snum += num_to_insert;
+                    break;
+            }
+        }
+    }
+
+    return snum_map[curr_len] = std::move(new_snums);
+
+} // static const std::vector<std::string>& find_strobogrammatic_nums( ...
 
 //! @brief First attempt to get all strobogrammatic numbers of length n
 //! @param[in] n Length of strobogrammatic number
@@ -23,21 +58,27 @@ static void find_strobogrammatic_nums(std::vector<std::string>& s_nums,
 static std::vector<std::string> findStrobogrammaticFA(int n)
 {
     //! @details leetcode.com/explore/interview/card/google/62/recursion-4/399
+    //!
+    //!          First attempt solution does not pass SampleTest3. Need to
+    //!          account for starting with zero if current length is not the
+    //!          final desired strobogrammatic number length
 
     if (n == 1)
     {
         return {"0", "1", "8"};
     }
-    
-    std::vector<std::string> s_nums;
 
-    for (const int num : {0, 1, 6, 8, 9})
+    if (n == 2)
     {
-        auto s_num = std::to_string(num);
-        find_strobogrammatic_nums(s_nums, s_num, n);
+        return {"11", "69", "88", "96"};
     }
 
-    return s_nums;
+    //! Map of <strobogrammatic number length, vector of all numbers>
+    //! Map memoizes subproblem solutions for n > 2
+    std::unordered_map<int, std::vector<std::string>> snum_map {
+        {1, {"0", "1", "8"}}, {2, {"00", "11", "69", "88", "96"}}};
+
+    return find_strobogrammatic_nums(snum_map, n);
 
 } // static std::vector<std::string> findStrobogrammaticFA( ...
 
@@ -53,4 +94,42 @@ TEST(FindStrobogrammaticTest, SampleTest2)
     const std::vector<std::string> expected_output {"0", "1", "8"};
 
     EXPECT_EQ(expected_output, findStrobogrammaticFA(1));
+}
+
+TEST(FindStrobogrammaticTest, SampleTest3)
+{
+    const std::vector<std::string> expected_output {
+        "100001", "101101", "106901", "108801", "109601", "110011", "111111",
+        "116911", "118811", "119611", "160091", "161191", "166991", "168891",
+        "169691", "180081", "181181", "186981", "188881", "189681", "190061",
+        "191161", "196961", "198861", "199661", "600009", "601109", "606909",
+        "608809", "609609", "610019", "611119", "616919", "618819", "619619",
+        "660099", "661199", "666999", "668899", "669699", "680089", "681189",
+        "686989", "688889", "689689", "690069", "691169", "696969", "698869",
+        "699669", "800008", "801108", "806908", "808808", "809608", "810018",
+        "811118", "816918", "818818", "819618", "860098", "861198", "866998",
+        "868898", "869698", "880088", "881188", "886988", "888888", "889688",
+        "890068", "891168", "896968", "898868", "899668", "900006", "901106",
+        "906906", "908806", "909606", "910016", "911116", "916916", "918816",
+        "919616", "960096", "961196", "966996", "968896", "969696", "980086",
+        "981186", "986986", "988886", "989686", "990066", "991166", "996966",
+        "998866", "999666"};
+
+    EXPECT_NE(expected_output, findStrobogrammaticFA(6));
+
+    const std::vector<std::string> first_attempt_output {
+        "110011", "111111", "116911", "118811", "119611", "160091", "161191",
+        "166991", "168891", "169691", "180081", "181181", "186981", "188881",
+        "189681", "190061", "191161", "196961", "198861", "199661", "610019",
+        "611119", "616919", "618819", "619619", "660099", "661199", "666999",
+        "668899", "669699", "680089", "681189", "686989", "688889", "689689",
+        "690069", "691169", "696969", "698869", "699669", "810018", "811118",
+        "816918", "818818", "819618", "860098", "861198", "866998", "868898",
+        "869698", "880088", "881188", "886988", "888888", "889688", "890068",
+        "891168", "896968", "898868", "899668", "910016", "911116", "916916",
+        "918816", "919616", "960096", "961196", "966996", "968896", "969696",
+        "980086", "981186", "986986", "988886", "989686", "990066", "991166",
+        "996966", "998866", "999666"};
+
+    EXPECT_EQ(first_attempt_output, findStrobogrammaticFA(6));
 }
