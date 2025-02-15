8142b13b3cae0ef1dd3602571c994699b3a65389
Jeffry <jeffryklew@gmail.com>
2025-02-14 22:40:36 -0500

    WIP Implement first attempt solution skeleton in strobogrammatic_number_ii.cpp

--- recursion/strobogrammatic_number_ii.cpp
+++ recursion/strobogrammatic_number_ii.cpp
@@ -3,6 +3,20 @@
 #include <string>
 #include <vector>
 
+static void find_strobogrammatic_nums(std::vector<std::string>& s_nums,
+                                      std::string&              s_num,
+                                      int                       s_num_len)
+{
+    if (std::ssize(s_num) == s_num_len)
+    {
+        s_nums.push_back(s_num);
+        return;
+    }
+
+    //! @todo Strobogrammatic number like bracket matching or anagram. Maybe use
+    //!       stack while backtracking to get correct order.
+}
+
 //! @brief First attempt to get all strobogrammatic numbers of length n
 //! @param[in] n Length of strobogrammatic number
 //! @return Vector of strobogrammatic numbers that are of length n
@@ -10,7 +24,20 @@ static std::vector<std::string> findStrobogrammaticFA(int n)
 {
     //! @details leetcode.com/explore/interview/card/google/62/recursion-4/399
 
-    //! @todo
+    if (n == 1)
+    {
+        return {"0", "1", "8"};
+    }
+    
+    std::vector<std::string> s_nums;
+
+    for (const int num : {0, 1, 6, 8, 9})
+    {
+        auto s_num = std::to_string(num);
+        find_strobogrammatic_nums(s_nums, s_num, n);
+    }
+
+    return s_nums;
 
 } // static std::vector<std::string> findStrobogrammaticFA( ...
 
