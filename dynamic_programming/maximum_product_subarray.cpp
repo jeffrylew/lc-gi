9d685cd48599301a89bcca7c4ee0b552e8803198
Jeffry Lew <jeffryklew@gmail.com>
2025-03-15 10:20:57 -0400

    Implement dynamic programming discussion solution in maximum_product_subarray.cpp

--- dynamic_programming/maximum_product_subarray.cpp
+++ dynamic_programming/maximum_product_subarray.cpp
@@ -142,14 +142,69 @@ static int maxProductDS1(std::vector<int> nums)
 
 } // static int maxProductDS1( ...
 
+//! @brief Dynamic programming discussion solution
+//! @param[in] nums Vector of integers
+//! @return Product of subarray with the largest product
+static int maxProductDS2(std::vector<int> nums)
+{
+    //! @details leetcode.com/problems/maximum-product-subarray/editorial
+    //!
+    //!          Time complexity O(N) where N = nums.size() to iterate through
+    //!          nums once.
+    //!          Space complexity O(1) for integer variables.
+
+    if (nums.empty())
+    {
+        return 0;
+    }
+
+    //! Maximum product up to current number. Tracks product of positive numbers
+    int curr_max_product {nums[0]};
+
+    //! Minimum product up to current number. Handles negative numbers
+    int curr_min_product {nums[0]};
+    int max_product {curr_max_product};
+
+    for (int idx = 1; idx < std::ssize(nums); ++idx)
+    {
+        const int curr_num {nums[idx]};
+
+        //! Take max value among
+        //! - curr_num: If accumulated product is worse than current number e.g.
+        //!             when preceded by zero [0, 4] or negative number [-3, 4]
+        //! - curr_max_product * curr_num: Picked when accumulated product has
+        //!                                been steadily increasing (positive)
+        //! - curr_min_product * curr_num: Picked if current number is negative
+        //!                                and accumulated product was negative
+        const int max_so_far {std::max({curr_num,
+                                        curr_max_product * curr_num,
+                                        curr_min_product * curr_num})};
+
+        curr_min_product = std::min({curr_num,
+                                     curr_max_product * curr_num,
+                                     curr_min_product * curr_num});
+
+        //! Update curr_max_product after curr_min_product to avoid overwriting
+        curr_max_product = max_so_far;
+
+        //! Update max_product with the maximum product found so far
+        max_product = std::max(max_product, curr_max_product);
+    }
+
+    return max_product;
+
+} // static int maxProductDS2( ...
+
 TEST(MaxProductTest, SampleTest1)
 {
     // EXPECT_EQ(6, maxProductFA({2, 3, -2, 4}));
     EXPECT_EQ(6, maxProductDS1({2, 3, -2, 4}));
+    EXPECT_EQ(6, maxProductDS2({2, 3, -2, 4}));
 }
 
 TEST(MaxProductTest, SampleTest2)
 {
     // EXPECT_EQ(0, maxProductFA({-2, 0, -1}));
     EXPECT_EQ(0, maxProductDS1({-2, 0, -1}));
+    EXPECT_EQ(0, maxProductDS2({-2, 0, -1}));
 }
