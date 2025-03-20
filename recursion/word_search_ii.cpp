ec4dbc036a106e4cb53456ad28173db7da2a5808
Jeffry Lew <jeffryklew@gmail.com>
2025-03-20 19:22:42 -0400

    Implement backtracking with trie discussion solution in recursion/word_search_ii.cpp

--- recursion/word_search_ii.cpp
+++ recursion/word_search_ii.cpp
@@ -139,6 +139,114 @@ static std::vector<std::string> findWordsFA(
 
 } // static std::vector<std::string> findWordsFA( ...
 
+struct Trie_node
+{
+    std::string_view word;
+
+    std::unordered_map<char, Trie_node> children;
+};
+
+static void get_words_at_pos_DS(int                             row,
+                                int                             col,
+                                Trie_node&                      parent,
+                                std::vector<std::vector<char>>& board_in,
+                                std::vector<std::string>&       words_on_board)
+{
+    const char orig_letter {board_in[row][col]};
+    auto&      curr_node = parent.children[orig_letter];
+
+    //! Check if there is a matching word
+    if (!curr_node.word.empty())
+    {
+        words_on_board.emplace_back(curr_node.word);
+
+        //! Prevent duplicate entries
+        curr_node.word = {};
+    }
+
+    //! Mark the current letter as visited before checking neighbors
+    board_in[row][col] = '#';
+
+    //! Explore neighbor cells clockwise: up, right, down, left
+    const std::vector<std::pair<int, int>> directions {
+        {-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+
+    for (const auto& [row_offset, col_offset] : directions)
+    {
+        const int next_row {row + row_offset};
+        const int next_col {col + col_offset};
+        if (next_row < 0
+            || next_row >= std::ssize(board_in)
+            || next_col < 0
+            || next_col >= std::ssize(board_in[0]))
+        {
+            continue;
+        }
+
+        if (curr_node.children.contains(board_in[next_row][next_col]))
+        {
+            get_words_at_pos_DS(next_row,
+                                next_col,
+                                curr_node,
+                                board_in,
+                                words_on_board);
+        }
+    }
+
+    //! End of checking neighbors, restore the original letter
+    board_in[row][col] = orig_letter;
+
+    //! Optimization: Incrementally remove the leaf nodes
+    if (curr_node.children.empty())
+    {
+        parent.children.erase(orig_letter);
+    }
+
+} // static void get_words_at_pos_DS( ...
+
+//! @brief Backtracking with Trie discussion solution
+//! @param[in] board Reference to m x n board of characters
+//! @param[in] words Reference to vector of strings to search for
+//! @return Vector of words from list that were found in board
+static std::vector<std::string> findWordsDS(
+    const std::vector<std::vector<char>>& board,
+    const std::vector<std::string>&       words)
+{
+    //! @details https://leetcode.com/problems/word-search-ii/
+
+    //! Step 1) Construct the Trie
+    Trie_node root;
+    for (const auto& word : words)
+    {
+        auto* node = &root;
+        for (const char letter : word)
+        {
+            node = &node->children[letter];
+        }
+
+        //! Store word in Trie
+        node->word = word;
+    }
+
+    auto                     board_cpy = board;
+    std::vector<std::string> words_on_board;
+
+    //! Step 2) Start backtracking from each cell in the board
+    for (int row = 0; row < std::ssize(board); ++row)
+    {
+        for (int col = 0; col < std::ssize(board[0]); ++col)
+        {
+            if (root.children.contains(board[row][col]))
+            {
+                get_words_at_pos_DS(row, col, root, board_cpy, words_on_board);
+            }
+        }
+    }
+
+    return words_on_board;
+
+} // static std::vector<std::string> findWordsDS( ...
+
 TEST(FindWordsTest, SampleTest1)
 {
     const std::vector<std::vector<char>> board {
@@ -151,6 +259,7 @@ TEST(FindWordsTest, SampleTest1)
     const std::vector<std::string> expected_output {"eat", "oath"};
 
     EXPECT_EQ(expected_output, findWordsFA(board, words));
+    EXPECT_EQ(expected_output, findWordsDS(board, words));
 }
 
 TEST(FindWordsTest, SampleTest2)
@@ -159,6 +268,7 @@ TEST(FindWordsTest, SampleTest2)
     const std::vector<std::string>       words {"abcb"};
 
     EXPECT_TRUE(findWordsFA(board, words).empty());
+    EXPECT_TRUE(findWordsDS(board, words).empty());
 }
 
 TEST(FindWordsTest, SampleTest3)
@@ -168,6 +278,7 @@ TEST(FindWordsTest, SampleTest3)
     const std::vector<std::string>       expected_output {"a"};
 
     EXPECT_EQ(expected_output, findWordsFA(board, words));
+    EXPECT_EQ(expected_output, findWordsDS(board, words));
 }
 
 TEST(FindWordsTest, SampleTest4)
@@ -182,6 +293,7 @@ TEST(FindWordsTest, SampleTest4)
     const std::vector<std::string> expected_output {"oa", "oaa"};
 
     EXPECT_EQ(expected_output, findWordsFA(board, words));
+    EXPECT_EQ(expected_output, findWordsDS(board, words));
 }
 
 TEST(FindWordsTest, SampleTest5)
@@ -197,4 +309,5 @@ TEST(FindWordsTest, SampleTest5)
 
     EXPECT_NE(expected_output, findWordsFA(board, words));
     EXPECT_EQ(FA_output, findWordsFA(board, words));
+    EXPECT_EQ(expected_output, findWordsDS(board, words));
 }
