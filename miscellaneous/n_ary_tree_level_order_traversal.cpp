c814f4be3af0ebfbe94513a4f4896a4ee02e7a77
Jeffry Lew <jeffryklew@gmail.com>
2025-04-14 17:11:07 -0400

    Implement first attempt solution in n_ary_tree_level_order_traversal.cpp

---
+++ miscellaneous/n_ary_tree_level_order_traversal.cpp
@@ -0,0 +1,96 @@
+#include "NaryNode.hpp"
+
+#include <gtest/gtest.h>
+
+#include <queue>
+#include <utility>
+#include <vector>
+
+//! @brief First attempt to get the level order traversal of an n-ary tree
+//! @param[in] root Pointer to an NaryNode that is the root of an n-ary tree
+//! @return Vector of node values per level
+static std::vector<std::vector<int>> levelOrderFA(NaryNode* root)
+{
+    //! @details leetcode.com/explore/learn/card/n-ary-tree/130/traversal/915
+    //!
+    //!          Time complexity O(N) where N = number of nodes in n-ary tree.
+    //!          Space complexity O(N) in the worst case where the root has N-1
+    //!          children.
+
+    std::vector<std::vector<int>> node_values;
+    std::queue<NaryNode*>         nary_nodes;
+
+    if (root == nullptr)
+    {
+        return node_values;
+    }
+
+    nary_nodes.push(root);
+    while (!nary_nodes.empty())
+    {
+        const auto num_nodes = static_cast<int>(std::ssize(nary_nodes));
+
+        std::vector<int> curr_level_node_values(num_nodes);
+
+        for (int node_idx = 0; node_idx < num_nodes; ++node_idx)
+        {
+            const auto node_ptr = nary_nodes.front();
+            nary_nodes.pop();
+
+            curr_level_node_values[node_idx] = node_ptr->val;
+
+            for (const auto& child_ptr : node_ptr->children)
+            {
+                nary_nodes.push(child_ptr);
+            }
+        }
+        
+        node_values.push_back(std::move(curr_level_node_values));
+    }
+
+    return node_values;
+
+} // static std::vector<std::vector<int>> levelOrderFA( ...
+
+TEST(NaryLevelOrderTest, SampleTest1)
+{
+    constexpr NaryNode two {2};
+    constexpr NaryNode four {4};
+    constexpr NaryNode five {5};
+    constexpr NaryNode six {6};
+
+    const NaryNode three {3, std::vector<NaryNode*> {&five, &six}};
+    const NaryNode one {1, std::vector<NaryNode*> {&three, &two, &four}};
+
+    const std::vector<std::vector<int>> expected_output {
+        {1}, {3, 2, 4}, {5, 6}};
+
+    EXPECT_EQ(expected_output, levelOrderFA(&one));
+}
+
+TEST(NaryLevelOrderTest, SampleTest2)
+{
+    constexpr NaryNode two {2};
+    constexpr NaryNode six {6};
+    constexpr NaryNode ten {10};
+    constexpr NaryNode twelve {12};
+    constexpr NaryNode thirteen {13};
+    constexpr NaryNode fourteen {14};
+
+    const NaryNode eight {8, std::vector<NaryNode*> {&twelve}};
+    const NaryNode nine {9, std::vector<NaryNode*> {&thirteen}};
+    const NaryNode eleven {11, std::vector<NaryNode*> {&fourteen}};
+
+    const NaryNode four {4, std::vector<NaryNode*> {&eight}};
+    const NaryNode seven {7, std::vector<NaryNode*> {&eleven}};
+
+    const NaryNode three {3, std::vector<NaryNode*> {&six, &seven}};
+    const NaryNode five {5, std::vector<NaryNode*> {&nine, &ten}};
+
+    const NaryNode one {1, std::vector<NaryNode*> {&two, &three, &four, &five}};
+
+    const std::vector<int> expected_output {
+        {1}, {2, 3, 4, 5}, {6, 7, 8, 9, 10}, {11, 12, 13}, {14}};
+
+    EXPECT_EQ(expected_output, levelOrderFA(&one));
+}
