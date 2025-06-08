97e2440229dc4beeea8a3e3ff2cba309fe423a9f
Jeffry Lew <jeffryklew@gmail.com>
2025-06-08 13:29:43 -0400

    Finish first attempt solution in arrays_and_strings/trapping_rain_water.cpp

--- arrays_and_strings/trapping_rain_water.cpp
+++ arrays_and_strings/trapping_rain_water.cpp
@@ -1,44 +1,65 @@
 #include <gtest/gtest.h>
 
 #include <algorithm>
-#include <queue>
+#include <deque>
+#include <iterator>
 #include <vector>
 
 //! @brief First attempt to compute how much water can be trapped after rain
-//! @param[in] Vector of N non-negative integers representing an elevation map
+//! @param[in] height Vector of N non-negative ints representing elevation map
 //! @return Units of rain water that can be trapped
 static int trapFA(const std::vector<int>& height)
 {
+    //! @details https://leetcode.com/explore/interview/card/google/59
+    //!          /array-and-strings/341/
+    //!
+    //!          Passes 222/324 test cases. Does not pass SampleTest3.
+    //!          Time complexity O(N), where N = Number of heights. We loop
+    //!          through the vector at most twice.
+    //!          Space complexity O(N) for height_deque and remaining_heights.
+
     int water_units {};
 
-    //! Monotonically decreasing queue of heights based on their indices
-    std::queue<int> height_indices;
-    const auto      num_heights = static_cast<int>(std::ssize(height));
+    //! Deque of heights
+    std::deque<int> height_deque;
 
-    for (int height_idx = 0; height_idx < num_heights; ++height_idx)
+    for (const int curr_height : height)
     {
-        while (!height_indices.empty()
-               && height[height_idx] > height[height_idx_stack.top()])
+        while (!height_deque.empty()
+               && curr_height > height_deque.back()
+               && curr_height >= height_deque.front())
         {
-            if (height[height_idx_stack.top()] > 0)
+            const int water_level {std::min(height_deque.front(), curr_height)};
+
+            if (water_level > 0 && water_level != height_deque.back())
             {
-                left_idx = std::min(left_idx, height_idx_stack.top());
+                water_units += water_level - height_deque.back();
             }
 
-            height_idx_stack.pop();
+            height_deque.pop_back();
         }
 
-        if (left_idx != num_heights)
+        height_deque.push_back(curr_height);
+    }
+
+    std::vector<int> remaining_heights(std::move_iterator(height_deque.begin()),
+                                       std::move_iterator(height_deque.end()));
+    height_deque.clear();
+
+    for (const int curr_height : remaining_heights)
+    {
+        while (!height_deque.empty() && curr_height > height_deque.back())
         {
-            const int 
-            water_units += (height_idx - )
+            water_units += curr_height - height_deque.back();
+            height_deque.pop_back();
         }
 
-        height_idx_stack.push(height_idx);
+        height_deque.push_back(curr_height);
     }
 
     return water_units;
-}
+
+} // static int trapFA( ...
 
 TEST(TrapTest, SampleTest1)
 {
@@ -49,7 +70,27 @@ TEST(TrapTest, SampleTest1)
 
 TEST(TrapTest, SampleTest2)
 {
+    //           -
+    // - 1 2 3 4 -
+    // - 5 6 - 7 -
+    // - - 8 - - -
+    // - - 9 - - -
     const std::vector<int> height {4, 2, 0, 3, 2, 5};
 
     EXPECT_EQ(9, trapFA(height));
+}
+
+TEST(TrapTest, SampleTest3)
+{
+    //   -
+    //   - 1 2 -
+    //   - 3 4 -
+    //   - 5 - -
+    //   - 6 - -
+    //   - 7 - -
+    //   - - - -
+    const std::vector<int> height {0, 7, 1, 4, 6};
+
+    EXPECT_NE(7, trapFA(height));
+    EXPECT_EQ(5, trapFA(height));
 }
