0dd6bfcffcfd18b7129b834eb5508ba2058f1007
Jeffry Lew <jeffryklew@gmail.com>
2025-07-07 22:00:37 -0400

    Implement sweep-line algorithm in miscellaneous/my_calendar_iii.cpp

--- miscellaneous/my_calendar_iii.cpp
+++ miscellaneous/my_calendar_iii.cpp
@@ -36,6 +36,40 @@ private:
     int max_intersections {};
 };
 
+//! @class MyCalendarThreeDS1
+//! @brief Sweep-line algorithm discussion solution
+//! @details https://leetcode.com/problems/my-calendar-iii/editorial/
+//!
+//!          Time complexity O(N ^ 2) where N = number of calls to book. For
+//!          each new event, we update changes at two time points in O(log N).
+//!          Then we traverse differential_at_time_point in O(N) for each of the
+//!          N calls to book().
+//!          Space complexity O(N) for the differential_at_time_point map.
+class MyCalendarThreeDS1
+{
+public:
+    int book(int startTime, int endTime)
+    {
+        int num_intervals_at_current_time {};
+        int max_intersections {};
+
+        ++differential_at_time_point[startTime];
+        --differential_at_time_point[endTime];
+
+        for (const auto& [time, delta_count] : differential_at_time_point)
+        {
+            num_intervals_at_current_time += delta_count;
+            max_intersections = std::max(max_intersections,
+                                         num_intervals_at_current_time);
+        }
+
+        return max_intersections;
+    }
+
+private:
+    std::map<int, int> differential_at_time_point;
+};
+
 TEST(MyCalendarThreeTest, SampleTest1)
 {
     MyCalendarThreeFA my_calendar_three_fa;
@@ -46,4 +80,13 @@ TEST(MyCalendarThreeTest, SampleTest1)
     EXPECT_EQ(3, my_calendar_three_fa.book(5, 15));
     EXPECT_EQ(3, my_calendar_three_fa.book(5, 10));
     EXPECT_EQ(3, my_calendar_three_fa.book(25, 55));
+
+    MyCalendarThreeDS1 my_calendar_three_ds1;
+
+    EXPECT_EQ(1, my_calendar_three_ds1.book(10, 20));
+    EXPECT_EQ(1, my_calendar_three_ds1.book(50, 60));
+    EXPECT_EQ(2, my_calendar_three_ds1.book(10, 40));
+    EXPECT_EQ(3, my_calendar_three_ds1.book(5, 15));
+    EXPECT_EQ(3, my_calendar_three_ds1.book(5, 10));
+    EXPECT_EQ(3, my_calendar_three_ds1.book(25, 55));
 }
