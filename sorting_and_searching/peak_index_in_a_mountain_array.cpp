eaeb8945bb6de8c9c3e1c09495ea3f0569acb232
Jeffry Lew <jeffryklew@gmail.com>
2025-07-12 14:26:45 -0400

    Implement first attempt solution in peak_index_in_a_mountain_array.cpp

---
+++ sorting_and_searching/peak_index_in_a_mountain_array.cpp
@@ -0,0 +1,62 @@
+#include <gtest/gtest.h>
+
+#include <vector>
+
+//! @brief First attempt to get index of peak element
+//! @param[in] arr Vector of ints forming a mountain
+//! @return Index of the peak element
+static int peakIndexInMountainArrayFA(std::vector<int> arr)
+{
+    //! @details https://leetcode.com/explore/interview/card/google/63
+    //!          /sorting-and-searching-4/3084/
+    //!
+    //!          Time complexity O(log N) where N = arr.size() for binary search
+    //!          Space complexity O(1)
+
+    const auto arr_size = static_cast<int>(std::ssize(arr));
+
+    int left_idx {};
+    int peak_idx {arr.front() >= arr.back() ? 0 : arr_size - 1};
+    int right_idx {arr_size - 1};
+
+    while (left_idx < right_idx)
+    {
+        const int mid_idx {left_idx + (right_idx - left_idx) / 2};
+
+        const int prev_elem {arr[mid_idx - 1]};
+        const int curr_elem {arr[mid_idx]};
+        const int next_elem {arr[mid_idx + 1]};
+
+        if (prev_elem < curr_elem && next_elem < curr_elem)
+        {
+            peak_idx = mid_idx;
+            break;
+        }
+
+        if (prev_elem < next_elem)
+        {
+            left_idx = mid_idx;
+        }
+        else
+        {
+            right_idx = mid_idx;
+        }
+    }
+
+    return peak_idx;
+}
+
+TEST(PeakIndexInMountainArrayTest, SampleTest1)
+{
+    EXPECT_EQ(1, peakIndexInMountainArrayFA({0, 1, 0}));
+}
+
+TEST(PeakIndexInMountainArrayTest, SampleTest2)
+{
+    EXPECT_EQ(1, peakIndexInMountainArrayFA({0, 2, 1, 0}));
+}
+
+TEST(PeakIndexInMountainArrayTest, SampleTest3)
+{
+    EXPECT_EQ(1, peakIndexInMountainArrayFA({0, 10, 5, 2}));
+}
