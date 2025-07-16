315250382a3085b5e81e8f4447489e1aee72b0f7
Jeffry Lew <jeffryklew@gmail.com>
2025-07-16 19:33:43 -0400

    Implement linear scan discussion solution in peak_index_in_a_mountain_array.cpp

--- sorting_and_searching/peak_index_in_a_mountain_array.cpp
+++ sorting_and_searching/peak_index_in_a_mountain_array.cpp
@@ -46,17 +46,43 @@ static int peakIndexInMountainArrayFA(std::vector<int> arr)
     return peak_idx;
 }
 
+//! @brief Linear scan discussion solution
+//! @param[in] arr Vector of ints forming a mountain
+//! @return Index of the peak element
+static int peakIndexInMountainArrayDS1(std::vector<int> arr)
+{
+    //! @details https://leetcode.com/problems/peak-index-in-a-mountain-array
+    //!
+    //!          Time complexity O(N). We do a linear scan and compare adjacent
+    //!          elements until we get the peak of the mountain. In the worst
+    //!          case, the peak is the second-to-last element of arr which would
+    //!          result in O(N).
+    //!          Space complexity O(1) for the integer idx.
+
+    int idx {};
+
+    while (arr[idx] < arr[idx + 1])
+    {
+        ++idx;
+    }
+
+    return idx;
+}
+
 TEST(PeakIndexInMountainArrayTest, SampleTest1)
 {
     EXPECT_EQ(1, peakIndexInMountainArrayFA({0, 1, 0}));
+    EXPECT_EQ(1, peakIndexInMountainArrayDS1({0, 1, 0}));
 }
 
 TEST(PeakIndexInMountainArrayTest, SampleTest2)
 {
     EXPECT_EQ(1, peakIndexInMountainArrayFA({0, 2, 1, 0}));
+    EXPECT_EQ(1, peakIndexInMountainArrayDS1({0, 2, 1, 0}));
 }
 
 TEST(PeakIndexInMountainArrayTest, SampleTest3)
 {
     EXPECT_EQ(1, peakIndexInMountainArrayFA({0, 10, 5, 2}));
+    EXPECT_EQ(1, peakIndexInMountainArrayDS1({0, 10, 5, 2}));
 }
