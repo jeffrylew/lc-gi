bff2cb78418602de10c8c6ea2110690e5fd94d59
Jeffry Lew <jeffryklew@gmail.com>
2026-07-14 18:29:43 -0400

    Add big O analysis to make a rotated copy discussion solution in other/strobogrammatic_number.cpp

--- other/strobogrammatic_number.cpp
+++ other/strobogrammatic_number.cpp
@@ -63,9 +63,15 @@ static bool isStrobogrammaticFA(const std::string& num)
 //! @brief Discussion solution for checking if num is a strobogrammatic number
 //! @param[in] num String that represents an integer of length up to 50
 //! @return True if num looks the same when rotated 180 degrees, else false
-static bool isStrobogrammaticDS(const std::string& num)
+static bool isStrobogrammaticDS1(const std::string& num)
 {
     //! @details https://leetcode.com/problems/strobogrammatic-number/editorial/
+    //!
+    //!          Time complexity O(N) where N = num.size(). In the worst case,
+    //!          all digits in num will be rotatable. For each digit, we look up
+    //!          its rotation in O(1). In the final step, comparing two strings
+    //!          of length N costs O(N).
+    //!          Space complexity O(N) for rotated_num.
 
     std::unordered_map<char, char> rotated_digits {
         {'0', '0'}, {'1', '1'}, {'6', '9'}, {'8', '8'}, {'9', '6'}};
@@ -89,29 +95,29 @@ static bool isStrobogrammaticDS(const std::string& num)
 TEST(IsStrobogrammatic, SampleTest1)
 {
     EXPECT_TRUE(isStrobogrammaticFA("69"));
-    EXPECT_TRUE(isStrobogrammaticDS("69"));
+    EXPECT_TRUE(isStrobogrammaticDS1("69"));
 }
 
 TEST(IsStrobogrammatic, SampleTest2)
 {
     EXPECT_TRUE(isStrobogrammaticFA("88"));
-    EXPECT_TRUE(isStrobogrammaticDS("88"));
+    EXPECT_TRUE(isStrobogrammaticDS1("88"));
 }
 
 TEST(IsStrobogrammatic, SampleTest3)
 {
     EXPECT_TRUE(isStrobogrammaticFA("962"));
-    EXPECT_TRUE(isStrobogrammaticDS("962"));
+    EXPECT_TRUE(isStrobogrammaticDS1("962"));
 }
 
 TEST(IsStrobogrammatic, SampleTest4)
 {
     EXPECT_FALSE(isStrobogrammaticFA("6"));
-    EXPECT_FALSE(isStrobogrammaticDS("6"));
+    EXPECT_FALSE(isStrobogrammaticDS1("6"));
 }
 
 TEST(IsStrobogrammatic, SampleTest5)
 {
     EXPECT_FALSE(isStrobogrammaticFA("868"));
-    EXPECT_FALSE(isStrobogrammaticDS("868"));
+    EXPECT_FALSE(isStrobogrammaticDS1("868"));
 }
