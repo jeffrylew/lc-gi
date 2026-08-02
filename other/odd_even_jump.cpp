5676c5b8bfe1ac4066a9a5ca61d63e86f60f6e8a
Jeffry Lew <jeffryklew@gmail.com>
2026-08-01 21:33:01 -0400

    Add explanation to Example 2 in other/odd_even_jump.cpp

--- other/odd_even_jump.cpp
+++ other/odd_even_jump.cpp
@@ -33,6 +33,23 @@ TEST(OddEvenJumpsTest, Example2)
     //!                  index  0  1  2  3  4
     const std::vector<int> arr {2, 3, 1, 1, 4};
 
+    /*
+     Starting index 0:
+       - Make jumps to indices 1, 2, 3
+       - During 1st jump (odd), jump to arr[1] since it is the smallest value in
+         arr[1], arr[2], arr[3], arr[4] that is greater than or equal to arr[0]
+       - During 2nd jump (even), jump from arr[1] to arr[2] since arr[2] is the
+         largest value in arr[2], arr[3], arr[4] that is <= arr[1]. arr[3] is
+         also the largest value but 2 is a smaller index so we only jump to 2
+       - During 3rd jump (odd), jump from arr[2] to arr[3] since arr[3] is the
+         smallest value in arr[3], arr[4] that is >= arr[2].
+       - Can't jump from arr[3] to arr[4] so starting index 0 is not good
+     Starting index 1: Jump to arr[4] so can reach the end
+     Starting index 2: Jump to arr[3] and then can't jump any more
+     Starting index 3: Jump to arr[4] so can reach the end
+     Starting index 4: Already at the end
+     Thus, there are 3 good starting indices (1, 3, 4) where can reach the end
+     */
     EXPECT_EQ(3, oddEvenJumpsFA(arr));
 }
 
