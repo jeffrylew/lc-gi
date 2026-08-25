0631ff340e314447054944a1d66242334e4ebb56
Jeffry Lew <jeffryklew@gmail.com>
2026-08-24 23:16:52 -0400

    Implement first attempt solution in other/fruit_into_baskets.cpp

---
+++ other/fruit_into_baskets.cpp
@@ -0,0 +1,65 @@
+#include <gtest/gtest.h>
+
+#include <algorithm>
+#include <unordered_map>
+#include <vector>
+
+//! @brief First attempt to get max number of fruits that I can pick
+//! @param[in] fruits Reference to vector of trees where fruits[i] = fruit type
+//! @return Max number of fruits that I can pick from selecting two fruit types
+static int totalFruitFA(const std::vector<int>& fruits)
+{
+    //! @details leetcode.com/explore/interview/card/google/67/sql-2/3046
+
+    const auto num_trees = static_cast<int>(std::ssize(fruits));
+
+    //! Tracks unique fruit types and their counts within the sliding window
+    std::unordered_map<int, int> fruit_type_counts;
+
+    int max_num_fruits {};
+    int left_tree {};
+
+    for (int right_tree = 0; right_tree < num_trees; ++right_tree)
+    {
+        ++fruit_type_counts[fruits[right_tree]];
+
+        while (std::ssize(fruit_type_counts) > 2L)
+        {
+            const int left_fruit_type {fruits[left_tree]};
+
+            --fruit_type_counts[left_fruit_type];
+
+            if (fruit_type_counts[left_fruit_type] == 0)
+            {
+                fruit_type_counts.erase(left_fruit_type);
+            }
+
+            ++left_tree;
+        }
+
+        max_num_fruits = std::max(max_num_fruits, right_tree - left_tree + 1);
+    }
+
+    return max_num_fruits;
+}
+
+TEST(TotalFruitTest, SampleTest1)
+{
+    const std::vector<int> fruits {1, 2, 1};
+
+    EXPECT_EQ(3, totalFruitFA(fruits));
+}
+
+TEST(TotalFruitTest, SampleTest2)
+{
+    const std::vector<int> fruits {0, 1, 2, 2};
+
+    EXPECT_EQ(3, totalFruitFA(fruits));
+}
+
+TEST(TotalFruitTest, SampleTest3)
+{
+    const std::vector<int> fruits {1, 2, 3, 2, 2};
+
+    EXPECT_EQ(4, totalFruitFA(fruits));
+}
